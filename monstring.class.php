600a7e995f18fd3671fd6fac9ee5d326a9548a58
Marius Mandal <mariusmandal@gmail.com>
2013-09-07 17:13:55 +0200

    Pre2011 bah

--- monstring.class.php
+++ monstring.class.php
@@ -813,6 +813,7 @@ require_once 'UKM/statistikk.class.php';
 						FROM `smartukm_band` AS `band`
 						JOIN `smartukm_rel_pl_b` AS `pl_b` ON (`pl_b`.`b_id` = `band`.`b_id`)
 						WHERE `pl_b`.`pl_id` = '".$this->get('pl_id')."'";
+				var_dump($qry);
 				return $qry;
 			}
 
