c3971f8e8026b761ba3250e11dd22989c53d489a
jimtrim <jimfrodehoff@gmail.com>
2013-09-08 12:35:55 +0200

    add getUglyStatArrayPerson()

--- statistikk.class.php
+++ statistikk.class.php
@@ -99,6 +99,104 @@ class statistikk {
                 $this->_load($season);
 	}
 	
+	// Jim liker ikke denne
+	// henter personer
+	public function getUglyStatArrayPerson($season) {
+		if (!$this->type) return array();
+		
+		$missing = 0;
+		// Finner telling for hver bandtype (kategori)
+		$qry = "SELECT `bt_id`, COUNT(*) as count FROM `ukm_statistics`".
+				" WHERE `season` =#season AND `bt_id`>0";
+				
+		// finner telling for subkategorier i Scene
+		$subcat_qry = "SELECT `subcat`,COUNT(*) AS count FROM `ukm_statistics`".
+				" WHERE `season` =#season AND `bt_id = 1`".
+		
+		
+		if ($this->type == 'kommune') {
+			$qry .= " AND k_id IN #kommuner";
+			$subcat_qry = " and k_id IN #kommuner";
+			$query_pl_missing = "SELECT `pl_missing` as `missing`".
+								" FROM `smartukm_place` as `place`".
+								" JOIN `smartukm_rel_pl_k` as `rel`".
+								" ON `rel`.`pl_id` = `place`.`pl_id`".
+								" WHERE `rel`.`k_id` = #kommune".
+								" AND `place`.`season` = #season LIMIT 1";
+			
+		} else if ($this->type == 'fylke') {
+			$qry .= " AND `f_id` =#fylkeID";
+			$subcat_qry .=" AND `f_id` =#fylkeID";
+			$query_pl_missing = 
+				"SELECT SUM(`missing2`) AS `missing` FROM". 
+                    	" (SELECT `pl_missing` AS `missing2`, `pl_name`".
+					" FROM `smartukm_place` AS `pl` ".
+					" JOIN `smartukm_rel_pl_k` AS `rel`".
+					" ON (`rel`.`pl_id` = `pl`.`pl_id`)".
+					" JOIN `smartukm_kommune` AS `kommune`".
+					" ON (`kommune`.`id` = `rel`.`k_id`)".
+					" WHERE `kommune`.`idfylke` = #fylkeID".
+					" AND `pl_missing` > 0".
+					" AND `pl`.`season` = #season".
+					" GROUP BY `pl`.`pl_id`)".
+				" AS `temptable`";
+		} else if ($this->type == 'land') {
+			$qry .= " AND `f_id` != 21";
+			$subcat_qry .=" AND `f_id` != 21";
+			$query_pl_missing = "SELECT SUM(`pl_missing`) as `missing`".
+								" FROM `smartukm_place`".
+								" WHERE `season`=#season AND `pl_fylke` != 21";
+		} 
+		
+		$qry .= " GROUP BY `bt_id` ORDER BY `bt_id` asc; "; // asc er ikke viktig.
+		$qry_pl_missing .= " GROUP BY `subcat` ORDER BY `subcat` desc;"; // desc ER viktig!
+
+		// PL_missing
+		$sql = new SQL($query_pl_missing, array('season'=>(int)$season,
+											'fylkeID'=>(int)$this->fylkeID,
+											'kommune' => $this->kommuner[0],
+											'kommuner' => implode(',', $this->kommuner)));
+
+		$missing += (int)$sql->run('field', 'missing');
+
+		// stats
+		$sql = new SQL($qry, array('season'=>(int)$season,
+									'fylkeID'=>(int)$this->fylkeID,
+									'kommuner' => implode(',', $this->kommuner)));
+		$result = $sql->run();
+		
+		$array = array();
+		while ($r = mysql_fetch_assoc($result)) {
+			$array['bt_'.$r['bt_id']] = $r['count'];
+			
+			//subkategorier
+			if($r['bt_id'] == 1) {
+				$sql = new SQL($subcat_qry, array('season'=>(int)$season,
+										'fylkeID'=>(int)$this->fylkeID,
+										'kommuner' => implode(',', $this->kommuner)));
+				$subcat_result = $sql->run();
+				while ($sr = mysql_fetch_assoc($result)) {
+					if ($sr['subcat'] == "")
+						$array['annet'] += $sr['count'];
+					else
+						$array[$sr['subcat']] = $sr['count'];
+				}
+			} 
+		}
+
+
+
+
+
+
+// select COUNT(*),`bt_id` from `ukm_statistics` where `season` = 2009 AND `f_id` = 2 AND `bt_id` > 0 GROUP BY `bt_id` ORDER BY `bt_id` asc;
+// test-spørring: select * from `ukm_statistics` where `f_id` = 2 and `season` = 2009 and `bt_id` = 1;
+
+		// 1377		
+		
+	}
+	
+	
 	private function _load($season) {
                 $kommuneList = implode(', ', $this->kommuner);
 		
