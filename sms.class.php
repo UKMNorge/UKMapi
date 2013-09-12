abf3500ac60e92dc20d718d7bb99f03f9d7d4ffd
Marius Mandal <mariusmandal@gmail.com>
2013-09-12 14:58:03 +0200

    syntax err

--- sms.class.php
+++ sms.class.php
@@ -8,8 +8,8 @@
 require_once('UKM/sql.class.php');
 
 class SMS {
-	static var $price = 0.40;
-	static var $bogus = array(44444444, 99999999);
+	static $price = 0.40;
+	static $bogus = array(44444444, 99999999);
 	
 	var $error = false;
 	var error_messages = array();
