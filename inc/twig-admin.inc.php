384f40819cca0b9bc35a9c0813a171fe6c54f79e
Marius Mandal <mariusmandal@gmail.com>
2013-09-26 14:01:15 +0200

    twig template folder correction

--- inc/twig-admin.inc.php
+++ inc/twig-admin.inc.php
@@ -2,7 +2,7 @@
 function TWIG($template, $dataarray, $templatefolder) {
 	require_once('Twig/Autoloader.php');
 	Twig_Autoloader::register();
-	$loader = new Twig_Loader_Filesystem($templatefolder);
+	$loader = new Twig_Loader_Filesystem($templatefolder.'/twig/');
 	$twig = new Twig_Environment($loader);
 
 	return $twig->render($template, $dataarray);
