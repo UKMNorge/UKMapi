fea104249f8922611d263dc1e75104173824a341
Marius Mandal <mariusmandal@gmail.com>
2013-10-17 15:16:52 +0200

    URG-klasse

---
+++ urg.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class urg {
+	public function __construct($id) {
+		$sql = new SQL("SELECT * FROM `ukmno_urg`
+						WHERE `urg_id` = '#id'",
+						array('id' => $id));
+		$res = $sql->run('array');
+		foreach($res as $key => $val) {
+			$newkey = str_replace('urg_','', $key);
+			if(is_string($val))
+				$this->$newkey = utf8_encode($val);
+			else
+				$this->$newkey = $val;
+		}
+		
+		
+		$this->image = 'http://graph.facebook.com/'
+					.  str_replace('profile.php?id=','',$this->facebook)
+					.  '/picture?type=large';
+	}
+}
