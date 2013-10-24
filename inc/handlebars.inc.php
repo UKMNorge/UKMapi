d60765914258a4cecde2cc2b17160d4216c5af1f
Marius Mandal <mariusmandal@gmail.com>
2013-10-24 14:51:22 +0200

    debug

--- inc/handlebars.inc.php
+++ inc/handlebars.inc.php
@@ -5,11 +5,14 @@ function HANDLEBARS($templatefolder, $prefix='') {
 	$id = 'handlebars-'. ( empty($prefix) ? '' : $prefix .'-' );
 
 	$templates = glob($directory . "*.handlebars.html");
+	
+	var_dump($templatefolder);
+	var_dump($templates);
 	foreach($templates as $template) {
 		if($template != '.' && $template != '..') {
-			$SCRIPT .= '<script id="'. $id . $template .'" type="text/x-handlebars-template">'
+			$SCRIPT .= 'script id="'. $id . $template .'" type="text/x-handlebars-template">'
 					. file_get_contents( $templatefolder .'/'. $template )
-					. '</script>';
+					. '/script>';
 		}	
 	}
 	return $SCRIPT;
