e39ec0ff5e3de8970a1343159101c56bb24f2b26
Marius Mandal <mariusmandal@gmail.com>
2013-10-24 14:49:00 +0200

    syntax

--- inc/handlebars.inc.php
+++ inc/handlebars.inc.php
@@ -5,7 +5,7 @@ function HANDLEBARS($templatefolder, $prefix='') {
 	$id = 'handlebars-'. ( empty($prefix) ? '' : $prefix .'-' );
 
 	$templates = glob($directory . "*.txt");
-	foreach($$templates as $template) {
+	foreach($templates as $template) {
 		if($template != '.' && $template != '..') {
 			$SCRIPT .= '<script id="'. $id . $template .'" type="text/x-handlebars-template">'
 					. file_get_contents( $templatefolder .'/'. $template )
