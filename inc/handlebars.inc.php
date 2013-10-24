2c1e6e17a1d68064892ef81a0770bb61b506fb24
Marius Mandal <mariusmandal@gmail.com>
2013-10-24 14:46:41 +0200

    Create script tags for all handlebars templates

---
+++ inc/handlebars.inc.php
@@ -0,0 +1,16 @@
+<?php
+function HANDLEBARS($templatefolder, $prefix='') {
+	$SCRIPT = '';
+	
+	$id = 'handlebars-'. ( empty($prefix) ? '' : $prefix .'-' );
+
+	$templates = glob($directory . "*.txt");
+	foreach($$templates as $template) {
+		if($template != '.' && $template != '..') {
+			$SCRIPT .= '<script id="'. $id . $template .'" type="text/x-handlebars-template">'
+					. file_get_contents( $templatefolder .'/'. $template )
+					. '</script>';
+		}	
+	}
+	return $SCRIPT;
+}
