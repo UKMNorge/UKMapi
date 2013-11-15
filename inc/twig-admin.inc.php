c7edd21783cf6febf706fd6e3dbb083d13e18e08
Marius Mandal <mariusmandal@gmail.com>
2013-11-15 14:52:45 +0100

    Add twig debug support

--- inc/twig-admin.inc.php
+++ inc/twig-admin.inc.php
@@ -1,14 +1,17 @@
 <?php
-function TWIG($template, $dataarray, $templatefolder) {
+function TWIG($template, $dataarray, $templatefolder, $debug=false) {
 	require_once('Twig/Autoloader.php');
 	Twig_Autoloader::register();
 	$loader = new Twig_Loader_Filesystem($templatefolder.'/twig/');
-	$twig = new Twig_Environment($loader);
+	$twig = new Twig_Environment($loader, array('debug' => $debug));
+	
+	if($debug)
+		$twig->addExtension(new Twig_Extension_Debug());
 
 	return $twig->render($template, $dataarray);
 }
 
-function TWIGrender($template, $dataarray) {
-	return TWIG($template.'.twig.html', $dataarray, str_replace('/twig/','', TWIG_PATH));
+function TWIGrender($template, $dataarray, $debug=false) {
+	return TWIG($template.'.twig.html', $dataarray, str_replace('/twig/','', TWIG_PATH), $debug);
 }
 ?>
