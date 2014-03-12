7f825bb77d6e63d5f55575defe64c427de5d7d86
Marius Mandal <mariusmandal@gmail.com>
2014-03-12 11:43:57 +0100

    Påmeldingsikoner grayscale

--- monstring.class.php
+++ monstring.class.php
@@ -719,62 +719,74 @@ require_once 'UKM/statistikk.class.php';
                                             'name'=>'Film', 
                                             'ico'=>'video', 
                                             'ico_url'=>'http://ico.ukm.no/pamelding/video.png', 
+                                            'ico_url_gray'=>'http://ico.ukm.no/pamelding/video_gray.png', 
                                             'title'=>'Driver du og lager film, og har lyst til &aring; vise verden hva du driver med? Klikk her');
 			$BANDTYPES['regular'][] = array('bt_id'=>3, 
                                             'name'=>'Utstilling', 
                                             'ico'=>'utstilling', 
                                             'ico_url'=>'http://ico.ukm.no/pamelding/utstilling.png',  
+                                            'ico_url_gray'=>'http://ico.ukm.no/pamelding/utstilling_gray.png',
                                             'title'=>'Maler du bilder, er du fotograf, bygger du skulpturer? Klikk her');
 			$BANDTYPES['regular'][] = array('bt_id'=>1, 
                                             'name'=>'Musikk', 
                                             'ico'=>'scene', 
                                             'ico_url'=>'http://ico.ukm.no/pamelding/scene.png', 
+                                            'ico_url_gray'=>'http://ico.ukm.no/pamelding/scene_gray.png', 
                                             'title'=>'Synger du i dusjen, spiller du i band eller spiller du klaver? Det er plass til alle p&aring; UKM! Klikk her');
 			$BANDTYPES['regular'][] = array('bt_id'=>1, 
                                             'name'=>'Dans', 
                                             'ico'=>'dans', 
                                             'ico_url'=>'http://ico.ukm.no/pamelding/dans.png', 
+                                            'ico_url_gray'=>'http://ico.ukm.no/pamelding/dans_gray.png', 
                                             'title'=>'Jazz? Lyrisk? Hip-hop? Breakdance? Ballett? L&aelig;rt en dans p&aring; skolen? Laget din egen? Klikk her');
 			$BANDTYPES['regular'][] = array('bt_id'=>1, 
                                             'name'=>'Teater', 
                                             'ico'=>'teater', 
                                             'ico_url'=>'http://ico.ukm.no/pamelding/teater.png', 
+                                            'ico_url_gray'=>'http://ico.ukm.no/pamelding/teater_gray.png', 
                                             'title'=>'Bor det en skuespiller i deg som vil ut og opp p&aring; scenen? Nasjonalteateret neste, UKM f&oslash;rst! Klikk her');
 			$BANDTYPES['regular'][] = array('bt_id'=>1, 
                                             'name'=>'Litteratur', 
                                             'ico'=>'litteratur', 
                                             'ico_url'=>'http://ico.ukm.no/pamelding/litteratur.png', 
+                                            'ico_url_gray'=>'http://ico.ukm.no/pamelding/litteratur_gray.png', 
                                             'title'=>'Har du skrevet historien om deg selv i atten bind? Eller p&aring to linjer? Har du en korttekst i SMS-format? Klikk her');
 			$BANDTYPES['regular'][] = array('bt_id'=>1, 
                                             'name'=>'Annet p&aring; scenen',
                                             'ico'=>'annet', 
                                             'ico_url'=>'http://ico.ukm.no/pamelding/annet.png', 
+                                            'ico_url_gray'=>'http://ico.ukm.no/pamelding/annet_gray.png', 
                                             'title'=>'Driver du med en form for scenekunst som ikke passer i de andre kategoriene? Sjonglering? Flammesluking? Slangetemming? Klikk her');
 		
 			$BANDTYPES['regular'][] = array('bt_id'=>6, 
                                         'name'=>'Matkultur', 
                                         'ico'=>'matkultur', 
                                         'ico_url'=>'http://ico.ukm.no/pamelding/matkultur.png', 
+                                        'ico_url_gray'=>'http://ico.ukm.no/pamelding/matkultur_gray.png', 
                                         'title'=>'Lyst til &aring; imponere et dommerpanel med din egen kokkelering? Klikk her');
 			$BANDTYPES['work'][] = array('bt_id'=>5, 
                                          'name'=>'Nettredaksjon / Videoproduksjon', 
                                          'ico'=>'nettredaksjon', 
                                          'ico_url'=>'http://ico.ukm.no/pamelding/nettredaksjon.png', 
+                                         'ico_url_gray'=>'http://ico.ukm.no/pamelding/nettredaksjon_gray.png', 
                                          'title'=>'Har du lyst til &aring; jobbe som journalist under m&oslash;nstringen? Eller til &aring; v&aelig;re kameramann under en forestilling? Eller produsere hele showet? Klikk her og finn ut hvilke muligheter som finnes p&aring; din m&oslash;nstring!');
 			$BANDTYPES['work'][] = array('bt_id'=>4, 
                                          'name'=>'Konferansier', 
                                          'ico'=>'konferansier', 
                                          'ico_url'=>'http://ico.ukm.no/pamelding/konferansier.png', 
+                                         'ico_url_gray'=>'http://ico.ukm.no/pamelding/konferansier_gray.png', 
                                          'title'=>'Ikke redd for &aring; prate i store forsamlinger? Lyst til &aring; gj&oslash;re det med mikrofon og publikum i din hule h&aring;nd? Klikk her');
 			$BANDTYPES['work'][] = array('bt_id'=>8, 
                                          'name'=>'Arrang&oslash;r', 
                                          'ico'=>'arrangor', 
                                          'ico_url'=>'http://ico.ukm.no/pamelding/arrangor.png', 
+                                         'ico_url_gray'=>'http://ico.ukm.no/pamelding/arrangor_gray.png', 
                                          'title'=>'Lyst til &aring; bidra til &aring; lage m&oslash;nstring sammen med din lokale arrang&oslash;r? Klikk her');
 			$BANDTYPES['work'][] = array('bt_id'=>9, 
                                          'name'=>'Sceneteknikk', 
                                          'ico'=>'sceneteknikk', 
                                          'ico_url'=>'http://ico.ukm.no/pamelding/sceneteknikk.png', 
+                                         'ico_url_gray'=>'http://ico.ukm.no/pamelding/sceneteknikk_gray.png', 
                                          'title'=>'Liker du deg bedre bak scenen enn p&aring;? Har du lyst til &aring; bygge scene og rigge lyd og lys sammen med profesjonelle folk? Klikk her');
 			
 			return $BANDTYPES;
@@ -906,6 +918,7 @@ require_once 'UKM/statistikk.class.php';
 			## Looper alle innslag fra kommunen med den gitte statusen
 			$qry = new SQL($this->_load_innslag_qry($kommune, $status,$felt,$videresendte));
 			$res = $qry->run();
+			$bands = array();
 			if($res)
 			while($set = mysql_fetch_assoc($res)) {
 				## Hopper over hvis innslaget er logget avmeldt (og status tilfeldigvis glemt oppdatert (bug høsten 2011))
