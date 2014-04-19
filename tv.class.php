8646b489acb65a1b2c6943b966cf60b7dac2ca10
jardarn <jardar@ukm.no>
2014-04-19 13:59:17 +0200

    Update tv.class.php
    remove port 88

--- tv.class.php
+++ tv.class.php
@@ -156,7 +156,7 @@ class tv {
 		// Let videre etter filen
 		} else {
 			$UKMCURL = new UKMCURL();
-			$UKMCURL->port(88);
+			//$UKMCURL->port(88);
 			$UKMCURL->request($this->storageurl2
 								.'find.php'
 								.'?file='.$this->file_name
@@ -230,7 +230,7 @@ class tv {
 		$res = $UKMCURL->request( $this->storageurl.$this->img );
 		
 		if( $res == 404 ) {
-		$this->image_url = 'http://video2.ukm.no:88/'.$this->img;
+		$this->image_url = 'http://video2.ukm.no/'.$this->img;
 		} else {
 		$this->image_url = $this->storageurl.$this->img;
 		}
