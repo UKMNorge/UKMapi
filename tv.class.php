183ee1c07300e73f8e8da7d20ef04c886f857372
jardarn <jardar@ukm.no>
2014-04-29 10:14:44 +0200

    Update tv.class.php
    change ip

--- tv.class.php
+++ tv.class.php
@@ -8,7 +8,7 @@ class tv {
 	var $storageurl = 'http://video.ukm.no/';
 	var $storageIP	= '212.125.231.33';
 	var $storageurl2 = 'http://video2.ukm.no/';
-	var $storageIP2	= '195.18.146.12';
+	var $storageIP2	= '81.0.146.165';
 	
 	var $activeStorage = '';
 	
