606430c35982440b38d1c75fb027aae7e0c5522f
Marius Mandal <mariusmandal@gmail.com>
2014-05-20 10:31:46 +0200

    debug

--- inc/excel.inc.php
+++ inc/excel.inc.php
@@ -1,8 +1,8 @@
 <?php
-set_include_path ( '/usr/local/lib/php' );
+/*set_include_path ( '/usr/local/lib/php' );
 require_once('/usr/local/lib/php/PHPExcel/PHPExcel.php');
 require_once('PHPExcel/IOFactory.php');
-
+*/
 $objPHPExcel = new PHPExcel();
 
 if(!function_exists('i2a')) {
