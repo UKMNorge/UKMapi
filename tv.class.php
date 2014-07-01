a858f679f3df29f00ebf78e0b8f6edb130127a5e
jardarn <jardar@ukm.no>
2014-07-01 22:03:57 +0200

    Update tv.class.php
    source

--- tv.class.php
+++ tv.class.php
@@ -8,7 +8,7 @@ class tv {
 	var $storageurl = 'http://video.ukm.no/';
 	var $storageIP	= '212.125.231.33';
 	var $storageurl2 = 'http://video2.ukm.no/';
-	var $storageIP2	= '195.18.146.11';
+	var $storageIP2	= '81.0.146.164';
 	
 	var $activeStorage = '';
 	
