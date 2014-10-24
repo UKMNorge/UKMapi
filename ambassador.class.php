a3e9ce599dec5d174024d22ad4086e4c01024193
Marius Mandal <mariusmandal@gmail.com>
2014-10-24 12:37:00 +0200

    Oppdatert ambassadørklasse for symfony

--- ambassador.class.php
+++ ambassador.class.php
@@ -1,7 +1,10 @@
 <?php
 require_once('UKM/monstring.class.php');
+require_once('UKM/sql.class.php');
 
 class ambassador {
+	var $ID = false;
+	
 	public function __construct($face_id=false) {
 		$qry = new SQL("SELECT  *
 						FROM `ukm_ambassador` AS `amb`
@@ -12,7 +15,7 @@ class ambassador {
 
 		$res = $qry->run('array');
 
-		if(!$res && !$face_id)
+		if(!$res || !$face_id)
 			return false;
 		
 		if($face_id) {
@@ -28,8 +31,88 @@ class ambassador {
 			}
 			
 			$this->link = '//facebook.com/profile.php?id='.$this->faceID;
-			$this->image = '//graph.facebook.com/'.$this->faceID.'/picture';
+			$this->image = '//graph.facebook.com/'. $this->faceID .'/picture?type=large';
+			$this->thumb = '//graph.facebook.com/'.$this->faceID.'/picture';
+
 		}
+	}
+	
+	public function create( $faceID, $firstname, $lastname, $phone, $email, $gender, $birthday) {
+		
+		$qry = new SQL("SELECT `pl_id` FROM `ukm_ambassador_personal_invite`
+						WHERE `invite_phone` = '#phone'
+						AND `invite_confirmed` = 'true';",
+						array('phone'=>$phone));
+		$pl_id = $qry->run('field','pl_id');
+		
+		$create = new SQLins('ukm_ambassador');
+		$create->add('amb_faceID',		$faceID);
+		$create->add('amb_firstname', 	$firstname);
+		$create->add('amb_lastname', 	$lastname);
+		$create->add('amb_phone',		$phone);
+		$create->add('amb_birthday', 	$birthday);
+		$create->add('amb_email', 		$email);
+		$create->add('amb_gender', 		$gender);
+		$create->add('pl_id', 			$pl_id);
+		$res = $create->run();
+			
+		// OPPDATER INVITASJONEN
+		$invite = new SQLins('ukm_ambassador_personal_invite', array('invite_phone'=>$phone));
+		$invite->add('invite_confirmed', 'used');
+		$resinvite = $invite->run();
+		
+		$this->__construct( $faceID );
+		
+		return $this;
+	}
+	public function setAddress( $address, $postalcode, $postalplace ) {
+		$qry = new SQLins('ukm_ambassador_skjorte');
+		$qry->add('amb_id',		$this->getID());
+		$qry->add('adresse',	$address);
+		$qry->add('postnr',		$postalcode);
+		$qry->add('poststed',	$postalplace);
+		$res = $qry->run();
+	}
+
+	public function setSize( $size ) {
+		$qry = new SQLins('ukm_ambassador_skjorte', array('amb_ID' => $this->getId() ) );
+		$qry->add('amb_id',		$this->getId());
+		$qry->add('size', 		$size );
+		$res = $qry->run();
+	}
+	
+	public function getId() {
+		return $this->ID;
+	}
+	
+	public function getFacebookId() {
+		return $this->faceID;
+	}
+	
+	public function getFirstname() {
+		return $this->firstname;
+	}
+	public function getLastname() {
+		return $this->lastname;
+	}
+	public function getPhone() {
+		return $this->phone;
+	}
+	public function getEmail() {
+		return $this->email;
+	}
+	
+	public function getPlid() {
+		return $this->pl_id;
+	}
+	public function getMonstringName() {
+		return $this->monstring;
+	}
+	public function getImage() {
+		return $this->image;
+	}
+	public function getLink() {
+		return $this->link;
 	}
 	
 	public function delete() {
