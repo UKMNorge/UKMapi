4ed1b966b411b170147b5a77283a711e2a33edac
Asgeir Stavik Hustad <askhus@online.no>
2016-02-09 16:01:20 +0000

    Lagt til cloudflare.class.php

---
+++ cloudflare.class.php
@@ -0,0 +1,92 @@
+<?php
+require_once 'UKM/sql.class.php';
+require_once('UKM/curl.class.php');
+
+class cloudflare {
+	private $URL = UKM_CLOUDFLARE_URL;
+	private $ukmno_zone = UKM_CLOUDFLARE_UKMNO_ZONE;
+	private $AuthKey = UKM_CLOUDFLARE_AUTH_KEY;
+	private $email = UKM_CLOUDFLARE_EMAIL;
+	var $result = null;
+
+	// Denne funksjonen trigger en cloudflare-cache-rebuild
+	// Input er et enkelt filnavn eller et array med filnavn med hele URLen for access utenfra.
+	// Funksjonen returnerer true ved suksess, false ved feil.
+	// TODO: Logging ved feil.
+	public function purge($file = false) {
+		if (!$file) {
+			return false;
+		}
+		$purge_url = $this->URL . $this->ukmno_zone . '/purge_cache';
+		
+		$data = array();
+		if (is_array($file)) {
+			foreach ($file as $fil) {
+				if (!empty($fil))
+					$data['files'][] = $fil;
+			}
+		}
+		else
+			$data['files'][] = $file;
+
+		// Konfigurer CURL
+		$curl = new UKMCURL();
+		$curl->port(443);
+		#$curl->
+		$curl->requestType('DELETE');
+		$curl->addHeader('X-Auth-Email: '.$this->email);
+		$curl->addHeader('X-Auth-Key: '. $this->AuthKey);
+		$curl->json($data);
+		$this->result = $curl->request($purge_url);
+
+		if (isset($this->result->success) && $this->result->success) {
+	    	return true;
+	    }
+	    #var_dump($curl);
+	    #var_dump($res);
+	    #$curl->json
+	    $error = var_export($this->result->errors, true);
+	    if (is_array($file)) {
+	    	$file = implode($file);
+	    	error_log('Cloudflare cache-clear feilet på URLene: '.$file .' med feilmeldingene ' .$error);
+	    }
+	    else 
+	    	error_log('Cloudflare cache-clear feilet på URL: '.$file.' med feilmeldingen ' .$errors);
+	    return false;
+	}
+
+	public function purgeAll() {
+		$purge_url = $this->URL . $this->ukmno_zone . '/purge_cache';
+
+		$data['purge_everything'] = true;
+
+		// Konfigurer CURL
+		$curl = new UKMCURL();
+		$curl->port(443);
+		#$curl->
+		$curl->requestType('DELETE');
+		$curl->addHeader('X-Auth-Email: '.$this->email);
+		$curl->addHeader('X-Auth-Key: '. $this->AuthKey);
+		$curl->json($data);
+		$this->result = $curl->request($purge_url);
+
+		if (isset($this->result->success) && $this->result->success) {
+	    	return true;
+	    }
+	    #var_dump($curl);
+	    #var_dump($res);
+	    #$curl->json
+	    $error = var_export($this->result->errors, true);
+	    if (is_array($file)) {
+	    	$file = implode($file);
+	    	error_log('Cloudflare cache-clear feilet på URLene: '.$file .' med feilmeldingene ' .$error);
+	    }
+	    else 
+	    	error_log('Cloudflare cache-clear feilet på URL: '.$file.' med feilmeldingen ' .$errors);
+	    return false;
+	}
+
+	public function result() {
+		return $this->result;
+	}
+}
