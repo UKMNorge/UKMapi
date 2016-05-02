c72510a26722c0550da68afa5edd8c8cd7a02b99
root <root@ns1.ukm-dns.no>
2016-05-02 23:24:35 +0200

    Bruk riktig app

--- inc/dropbox.inc.php
+++ inc/dropbox.inc.php
@@ -5,12 +5,8 @@ define('DROPBOX_REDIR_URL', 'https://'. UKM_HOSTNAME .'/wp-content/plugins/UKMsy
 require_once('UKMconfig.inc.php');
 require_once('Dropbox/vendor/autoload.php');
 
-$dropboxKey = 'fwnfgdlcqnq77rv';
-$dropboxSecret = 'lcilu4dtwcl3b2l';
-$appName = 'UKMdigarkBilder/1.0';
-
-$appInfo = new Dropbox\AppInfo( $dropboxKey, $dropboxSecret );
+$appInfo = new Dropbox\AppInfo( DROPBOX_APP_ID, DROPBOX_APP_SECRET );
 // Temp-store CSRF Token
 $csrfTokenStore = new Dropbox\ArrayEntryStore( $_SESSION, 'dropbox-auth-csrf-token' );
 // Define auth-details
-$webAuth = new Dropbox\WebAuth( $appInfo, $appName, DROPBOX_REDIR_URL, $csrfTokenStore );
+$webAuth = new Dropbox\WebAuth( $appInfo, DROPBOX_APP_NAME, DROPBOX_REDIR_URL, $csrfTokenStore );
