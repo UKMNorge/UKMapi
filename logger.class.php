7ffed89aeb9338140356af0e070b27a0a9aff60f
Marius Mandal <mariusmandal@gmail.com>
2017-11-01 10:49:40 +0000

    Flyttet innslag_v1 og logger ut i egen fil

---
+++ logger.class.php
@@ -0,0 +1,78 @@
+<?php
+	
+class UKMlogger {
+	static $user = null;
+	static $system = null;
+	static $pl_id = null;
+	
+	static function log( $action, $object_id, $value ) {
+		if( !self::ready() ) {
+			throw new Exception('Logger incorrect set up.');
+		}
+		
+		$object = substr($action, 0, (strlen($action)-2));
+		
+		$sql = new SQLins('log_log');
+		$sql->add( 'log_u_id', self::getUser() );
+		$sql->add( 'log_system_id', self::getSystem() );
+		$sql->add( 'log_pl_id', self::getPlId() );
+
+		$sql->add( 'log_action', $action );
+		$sql->add( 'log_object', $object );
+		$sql->add( 'log_the_object_id', $object_id );
+
+		#$sql->add( 'log_time', $time ); // GJØRES AV MYSQL (DEFAULT: CURRENT_TIMESTAMP)
+		#echo $sql->debug();
+		$res = $sql->run();
+		if( 1 != $res ) {
+			throw new Exception("UKMlogger: Klarte ikke å logge til log_log! Feilmelding: ".$sql->error());
+		}
+
+		$id = $sql->insid();
+		
+		$sql = new SQLins('log_value');
+		$sql->add('log_id', $id);
+		$sql->add('log_value', $value);
+		$res = $sql->run();
+		if( 1 != $res ) {
+			throw new Exception("UKMlogger: Klarte ikke å logge til log_value!");
+		}
+
+		return true;
+	}
+	
+	static function ready() {
+		if( null == self::getUser() || null == self::getPlId() || null == self::getSystem() ) {
+			return false;
+		}
+		return true;
+	}
+	
+	static function setPlId( $pl_id ) {
+		self::$pl_id = $pl_id;
+	}
+	static function getPlId() {
+		return self::$pl_id;
+	}
+	
+	static function setUser( $user ) {
+		self::$user = $user;
+	}
+	static function getUser() {
+		return self::$user;
+	}
+	
+	static function setSystem( $system ) {
+		self::$system = $system;
+	}
+	static function getSystem() {
+		return self::$system;
+	}
+	
+	static function setID( $system, $user, $pl_id ) {
+		self::setSystem( $system );
+		self::setUser( $user );
+		self::setPlId( $pl_id );
+	}
+	
+}
--- write_innslag.class.php
+++ write_innslag.class.php
@@ -1,48 +1,6 @@
 <?php
 
-class UKMlogger {
-	var $user_id = null;
-	var $system_id = null;
-	
-	static function log( $action, $value ) {
-		$sql = new SQLins('smartukm_log_log');
-		$sql->add( self::getUser() );
-		$sql->add( self::getSystem() );
-		$sql->add( $action );
-		$sql->add( $value );
-		
-		$sql->debug();
-	}
-	
-	static function ready() {
-		return false;
-	}
-	
-	static function setUser( $user ) {
-		self::$user = $user;
-		return self;
-	}
-	static function getUser() {
-		return self::$user;
-	}
-	
-	static function setSystem( $system ) {
-		self::$system = $system;
-		return self;
-	}
-	static function getSystem() {
-		return self::$system;
-	}
-	
-	static function setID( $system, $user ) {
-		self::setSystem( $system );
-		self::setUser( $user );
-		return self;
-	}
-	
-}
-
-UKMlogger::setID( 'wordpress', '123' );
+require_once('UKM/log.class.php');
 
 class innslag_writeable extends innslag {
 	var $changes = array();
