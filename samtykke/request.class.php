b9d398ee795ab8d144d5a29809e8ac715851a23b
Marius Mandal <mariusmandal@gmail.com>
2018-02-20 19:38:57 +0000

    Lagt tilsamtykke-klasser

---
+++ samtykke/request.class.php
@@ -0,0 +1,244 @@
+<?php
+	
+class samtykke_request {
+	var $id;
+	var $prosjekt = null;
+	var $prosjekt_id;
+	var $fornavn;
+	var $etternavn;
+	var $mobil;
+	var $melding;
+	var $lenker;
+	var $hash;
+	var $timestamp;
+	
+	var $approval = null;
+	var $attributes = null;
+	
+	public function __construct( $id ) {
+		if( is_numeric( $id ) ) {
+			$this->_load_by_id( $id );
+		} elseif( is_array( $id ) ) {
+			$this->_load_by_row( $id );
+		} else {
+			throw new Exception('Kan kun laste inn samtykke-request med numerisk ID');
+		}
+	}
+	
+	private function _load_by_id( $id ) {
+		$sql = new SQL("
+			SELECT * 
+			FROM `samtykke_request`
+			WHERE `id` = '#id'",
+			[
+				'id' => $id
+			]
+		);
+		$sql->charset('UTF-8');
+		$res = $sql->run('array');
+		
+		if( $res ) {
+			$this->_load_by_row( $res );
+		}
+	}
+	
+	private function _load_by_row( $row ) {
+		$this->id = $row['id'];
+		$this->prosjekt_id = $row['prosjekt'];
+		$this->fornavn = $row['fornavn'];
+		$this->etternavn = $row['etternavn'];
+		$this->mobil = $row['mobil'];
+		$this->melding = $row['melding'];
+		$this->lenker = json_decode( $row['lenker'] );
+		$this->hash = $row['hash'];
+		$this->timestamp = $row['created'];
+	}
+	
+	
+	/**
+	 * Sett attributt
+	 * Sett egenskaper som for enkelhets skyld kan følge mønstringen et lite stykke
+	 * Vil aldri kunne lagres
+	 *
+	 * @param string $key
+	 * @param $value
+	 *
+	 * @return innslag
+	**/
+	public function setAttr( $key, $value ) {
+		if( null == $this->attributes ) {
+			$this->attributes = [];
+		}
+		$this->attributes[ $key ] = $value;
+		return $this;
+	}
+	
+	/**
+	 * Hent attributt
+	 *
+	 * @param string $key
+	 *
+	 * @return value
+	**/
+	public function getAttr( $key ) {
+		return isset( $this->attributes[ $key ] ) ? $this->attributes[ $key ] : false;
+	}
+	
+	
+	public function erGodkjent() {
+		try {
+			$this->getApproval();
+			// Over 15, trenger ikke foresattes godkjenning
+			if( !$this->getApproval()->trengerForesatt() ) {
+				return true;
+			}
+			// Under 15, trenger foresattes godkjenning
+			else {
+				try {
+					$godkjent = $this->getApproval()->getForesattApproval();
+					return true;
+				} catch( Exception $e ) {
+					if( $e->getCode() == 2 ) {
+						return false;
+					}
+					throw $e;
+				}
+			}
+		} catch( Exception $e ) {
+			if( $e->getCode() == 2 ) {
+				return false;
+			}
+			throw $e;
+		}
+		return false;
+	}
+	
+	public function harSvart() {
+		try {
+			$this->getApproval();
+			return true;
+		} catch( Exception $e ) {
+			if( $e->getCode() == 2 ) {
+				return false;
+			}
+			throw $e;
+		}
+		return false;
+	}
+	public function getId() {
+		return $this->id;
+	}
+	
+	public function getProsjektId() {
+		return $this->prosjekt_id;
+	}
+	public function getProsjekt() {
+		if( null == $this->prosjekt ) {
+			$this->prosjekt = new samtykke_prosjekt( $this->getProsjektId() );
+		}
+		return $this->prosjekt;
+	}
+	
+	public function getFornavn() {
+		return $this->fornavn;
+	}
+	public function getEtternavn() {
+		return $this->etternavn;
+	}
+	public function getMobil() {
+		return $this->mobil;
+	}
+	public function getMelding() {
+		return $this->melding;
+	}
+	public function getLenker() {
+		return $this->lenker;
+	}
+	public function getTimestamp() {
+		return $this->timestamp;
+	}
+	public function getHash() {
+		return $this->hash;
+	}
+	
+	public function getLenkeHash() {
+		return substr( $this->getHash(), 6, 10 );
+	}
+	
+	public function getApproval() {
+		require_once('UKM/samtykke/approval.class.php');
+		if( null == $this->approval ) {
+			$this->approval = new samtykke_approval( $this->getId() );
+		}
+		return $this->approval;
+	}
+
+	
+	public static function createMelding( $prosjekt, $melding, $lenker, $fornavn, $mobil, $hashexcerpt ) {
+		$lenke = 'https://samtykke.'. UKM_HOSTNAME .'/'. 
+			'?prosjekt='. $prosjekt->getLenkeHash() .
+			'&samtykke='. $hashexcerpt
+			;
+		
+		$search = [
+			'$navn',
+			'[lenke]',
+		];
+		
+		$replace = [
+			$fornavn,
+			$lenke
+		];
+		
+		return str_replace( $search, $replace, $melding );
+	}
+
+	public static function createMeldingForeldre( $request, $foresatt_navn, $foresatt_mobil ) {
+		$lenke = 'https://samtykke.'. UKM_HOSTNAME .'/'. 
+			'?prosjekt='. $request->getProsjekt()->getLenkeHash() .
+			'&samtykke='. $request->getLenkeHash() .
+			'&foresatt='. $foresatt_mobil
+			;
+		
+		return 'Hei! '. 
+			$request->getFornavn() .' har gitt sitt samtykke, men fordi '. $request->getFornavn() .' er under 15 år, '.
+			'trenger vi din godkjenning. Les mer og eventuelt gi ditt samtykke her: '.
+			$lenke
+			;
+	}
+	
+	public static function createMeldingTakk( $request ) {
+		if( $request->getApproval()->trengerForesatt() ) {
+			$hash = $request->getApproval()->getForesattApproval()->getLenkeHash();
+		} else {
+			$hash = $request->getApproval()->getLenkeHash();
+		}
+		return 'Takk, vi har nå mottatt ditt samtykke! '. "\r\n".
+			'Din kvittering: '. 
+			$request->getId() .'-'. $request->getLenkeHash() .'@'. $hash;
+	}
+
+	
+	public static function loadFromHash( $prosjekt, $samtykke ) {
+		require_once('UKM/samtykke/prosjekt.class.php');
+		
+		$sql = new SQL("SELECT * 
+			FROM `samtykke_prosjekt` AS `prosjekt`
+			JOIN `samtykke_request` AS `request` 
+				ON (`request`.`prosjekt` = `prosjekt`.`id`)
+			WHERE `prosjekt`.`hash-excerpt` = '#prosjekt'
+			AND `request`.`hash-excerpt` = '#samtykke'
+			",
+			[
+				'prosjekt' => $prosjekt,
+				'samtykke' => $samtykke
+			]
+		);
+		$res = $sql->run('array');
+		
+		if( is_array( $res ) ) {
+			return new samtykke_request( $res );
+		}
+		throw new Exception('Kunne ikke finne gitt samtykke-forespørsel');
+	}
+}
---
+++ samtykke/write.class.php
@@ -0,0 +1,175 @@
+<?php
+	
+class write_samtykke {
+	
+	public static function createProsjekt( $tittel ) {
+		require_once('UKM/samtykke/prosjekt.class.php');
+		$sql = new SQLins('samtykke_prosjekt');
+		$sql->add('tittel', $tittel);
+		$res = $sql->run();
+
+		$id = $sql->insId();
+			
+		if( $res == -1 ) {
+			$error = $sql->error();
+			if( strpos($error, 'Duplicate') == 0 ) {
+				throw new Exception('Et annet prosjekt med samme tittel finnes fra før');
+			}
+			throw new Exception('Kunne ikke opprette samtykke-prosjekt, ukjent feil');
+		}
+		if( is_numeric( $id ) ) {
+			return new samtykke_prosjekt( $id );
+		}
+		
+		throw new Exception('Kunne ikke opprette samtykke-prosjekt');
+	}
+	
+	public static function saveProsjekt( $prosjekt ) {
+		require_once('UKM/samtykke/prosjekt.class.php');
+		$prosjekt_db = new samtykke_prosjekt( $prosjekt->getId() );
+		
+		$sql = new SQLins(
+			'samtykke_prosjekt',
+			[
+				'id' => $prosjekt->getId()
+			]
+		);
+		// VERDIER SOM KAN OPPDATERES
+		$properties = [
+			'Tittel'		=> ['tittel'],
+			'Setning'		=> ['setning'],
+			'Varighet'		=> ['varighet'],
+			'Beskrivelse'	=> ['beskrivelse'],
+			'Hash'			=> ['hash'],
+		];
+		
+		// LOOP ALLE VERDIER, OG EVT LEGG TIL I SQL
+		foreach( $properties as $functionName => $db ) {
+			$function = 'get'.$functionName;
+			
+			if( $prosjekt_db->$function() != $prosjekt->$function() ) {
+				$sql->add( $db[0], $prosjekt->$function() );
+			}
+		}
+		$sql->add('hash-excerpt', substr( $prosjekt->getHash(), 6, 10 ) );
+		
+		if( !$sql->hasChanges() ) {
+			return true;
+		}
+		
+		$res = $sql->run();
+		if( $res ) {
+			return true;
+		}
+		
+		throw new Exception('Kunne ikke lagre samtykke-prosjekt');
+	}
+	
+	public static function lockProsjekt( $prosjekt ) {
+		
+		if( $prosjekt->isLocked() ) {
+			return true;
+		}
+		
+		$sql = new SQLins(
+			'samtykke_prosjekt',
+			[
+				'id' => $prosjekt->getId()
+			]
+		);
+		$sql->add('locked', 'true');
+		$res = $sql->run();
+		if( $res ) {
+			return true;
+		}
+		throw new Exception('Kunne ikke låse prosjektet!');
+	}
+	
+	
+	public static function createRequest( $prosjekt, $melding, $lenker, $fornavn, $etternavn, $mobil ) {
+		require_once('UKM/samtykke/request.class.php');
+
+		$hash = sha1( $fornavn .'-'. $etternavn .'-'. $mobil .'-'. var_export($lenker,true) );
+		$hashexcerpt = substr( $hash, 6, 10 );
+		$melding_fixed = samtykke_request::createMelding( $prosjekt, $melding, $lenker, $fornavn, $mobil, $hashexcerpt );
+		
+		
+		$sql = new SQLins('samtykke_request');
+		$sql->add('prosjekt', $prosjekt->getId() );
+		$sql->add('fornavn', $fornavn);
+		$sql->add('etternavn', $etternavn);
+		$sql->add('mobil', $mobil);
+		$sql->add('melding', $melding_fixed);
+		$sql->add('lenker', json_encode( $lenker ));
+		$sql->add('hash', $hash );
+		$sql->add('hash-excerpt', $hashexcerpt );
+		$res = $sql->run();
+		
+		return new samtykke_request( $sql->insId() );
+	}
+	
+	public static function godta( $request, $alder ) {
+		require_once('UKM/samtykke/prosjekt.class.php');
+		require_once('UKM/samtykke/request.class.php');
+		require_once('UKM/samtykke/approval.class.php');
+		
+		$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+		$hash = sha1( $request->getProsjektId() .'-'. $request->getId() .'-'. $alder .'-'. $ip );
+		$hashexcerpt = substr( $hash, 6, 10 );
+		
+		$sql = new SQLins('samtykke_approval');
+		$sql->add('prosjekt', $request->getProsjektId() );
+		$sql->add('request', $request->getId() );
+		$sql->add('prosjekt-request', $request->getProsjektId().'-'.$request->getId() );
+		$sql->add('alder', $alder);
+		if( $alder == 'over20' or (int) $alder >= 15 ) {
+			$sql->add('trenger_foresatt', 'false');
+		}
+		$sql->add('ip', $ip );
+		$sql->add('hash', $hash );
+		$sql->add('hash-excerpt', $hashexcerpt );
+		$res = $sql->run();
+		
+		return new samtykke_approval( $sql->insId() );
+	}
+	
+		
+	public static function godtaForesatt( $request ) {
+		require_once('UKM/samtykke/prosjekt.class.php');
+		require_once('UKM/samtykke/request.class.php');
+		require_once('UKM/samtykke/approval.class.php');
+		
+		$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+		$hash = sha1( $request->getId() .'-'. $request->getProsjektId() .'-'. $ip );
+		$hashexcerpt = substr( $hash, 6, 10 );
+		
+		$sql = new SQLins('samtykke_approval_foresatt');
+		$sql->add('approval', $request->getApproval()->getId() );
+		$sql->add('ip', $ip );
+		$sql->add('hash', $hash );
+		$sql->add('hash-excerpt', $hashexcerpt );
+		$res = $sql->run();
+		
+		return new samtykke_approval( $sql->insId() );
+	}
+	
+	
+	
+	public static function lagreForesatt( $request, $navn, $mobil ) {
+		require_once('UKM/samtykke/prosjekt.class.php');
+		require_once('UKM/samtykke/request.class.php');
+		require_once('UKM/samtykke/approval.class.php');
+
+		$sql = new SQLins(
+			'samtykke_approval', 
+			[
+				'request' => $request->getId(),
+				'prosjekt' => $request->getProsjektId()
+			]
+		);
+		$sql->add('foresatt_navn', $navn );
+		$sql->add('foresatt_mobil', $mobil );
+		
+		$res = $sql->run();
+	}
+}
