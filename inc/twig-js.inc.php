58c27eee189e69ce7b0adf3412c71d52a4626c61
Marius Mandal <mariusmandal@gmail.com>
2018-03-14 09:09:48 +0000

    TWIGjs støtter .html.twig også

--- inc/twig-js.inc.php
+++ inc/twig-js.inc.php
@@ -6,17 +6,18 @@ function TWIGjs($templatefolder, $prefix='') {
 	
 	$id = 'twigJS'. ( empty($prefix) ? '' : $prefix .'' );
 
-	$templates = glob($templatefolder . "*.twig.html");
+	$templates = glob($templatefolder . "*.twig*");
 	
 	foreach($templates as $template) {
 		if($template != '.' && $template != '..') {
-			$templateID = str_replace(array('.twig.html','_','.'),
-									  array('', '','_'),
+			$templateID = str_replace(array('.twig.html','.html.twig','_','.'),
+									  array('','','','_'),
 									  basename($template)
 									 );
 			$SCRIPT .= '<script type="text/javascript">'
 					. 'var '. $id.$templateID.' = twig({data: \''.str_replace("'","\'",preg_replace( "/\r|\n/", "",file_get_contents( $template ))).'\'});'
-					. '</script>';
+					. '</script>'
+					. "\r\n";
 		}
 	}
 	return $SCRIPT;
@@ -35,6 +36,8 @@ function TWIGjs_simple($templatefolder) {
 			$SCRIPT .= 'var twigJS_'. $templateID.' = twig({data: \''.str_replace("'","\'",preg_replace( "/\r|\n/", "",file_get_contents( $template ))).'\'});';
 		}
 	}
-	$SCRIPT .= '</script>';
+	$SCRIPT .= '</script>'
+		. "\r\n";
+;
 	return $SCRIPT;
 }
