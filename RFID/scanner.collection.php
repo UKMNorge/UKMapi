7b6c1e116d50478b3905b3be08c76783bb692804
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-05-30 08:52:49 +0000

    Add getByGUID

--- RFID/scanner.collection.php
+++ RFID/scanner.collection.php
@@ -8,4 +8,8 @@ require_once('scanner.class.php');
 class ScannerColl extends RFIDColl {
 	const TABLE_NAME = Scanner::TABLE_NAME;
 	public static $models = null;
+
+	public function getByGUID( $guid ) {
+		return parent::getByKey("GUID", $guid);
+	}
 }
