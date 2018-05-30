355a1f39d930a814c1a5cd23ea32c3da5402ea7a
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-05-30 12:07:54 +0200

    Add RFID-functions in person.collection.php

--- RFID/person.collection.php
+++ RFID/person.collection.php
@@ -9,6 +9,22 @@ class PersonColl extends RFIDColl {
 	const TABLE_NAME = Person::TABLE_NAME;
 	public static $models = null;
 	
+	public static function getByRFID( $rfid ) {
+		$row = POSTGRES::getRow("SELECT * FROM ". self::TABLE_NAME ." WHERE rfid=$1", [$id]);
+		
+		$object_class = str_replace('Coll', '', get_called_class());
+		return new $object_class( $row );
+	}
+	
+	public static function hasRFID( $rfid ) {
+		try {
+			$row = POSTGRES::getRow("SELECT * FROM ". self::TABLE_NAME ." WHERE rfid=$1", [$id]);
+			return true;
+		} catch( Exception $e ) {
+			return false;
+		}
+	}
+	
 	public static function getByForeignId( $id ) {
 		$row = POSTGRES::getRow("SELECT * FROM ". self::TABLE_NAME ." WHERE foreign_id=$1", [$id]);
 		
