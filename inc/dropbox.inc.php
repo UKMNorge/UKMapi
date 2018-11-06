a7fb2bea8956f65dbf15c01825680790b2ca7612
Marius Mandal <mariusmandal@gmail.com>
2018-11-07 00:05:04 +0100

    bruk ny og bedre autoload

--- inc/dropbox.inc.php
+++ inc/dropbox.inc.php
@@ -6,7 +6,7 @@ define('DROPBOX_APP_NAME', 'UKMdigark/1.0');
 define('DROPBOX_REDIR_URL', 'https://'. UKM_HOSTNAME .'/wp-content/plugins/UKMsystem_tools/dropbox.php');
 
 require_once('UKMconfig.inc.php');
-require_once('Dropbox/vendor/autoload.php');
+require_once('vendor/autoload.php');
 
 if( defined( 'DROPBOX_AUTH_ACCESS_TOKEN' ) ) {
 	$DROPBOX_APP = new DropboxApp(DROPBOX_APP_ID, DROPBOX_APP_SECRET, DROPBOX_AUTH_ACCESS_TOKEN);
