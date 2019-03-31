7b91956969a6649e4ee4a7feb9e98fea4200da67
Marius Mandal <mariusmandal@gmail.com>
2019-03-31 19:44:32 +0200

    Hello Konkurranse

---
+++ Konkurranse/config.class.php
@@ -0,0 +1,50 @@
+<?php
+
+
+require_once('UKM/_orm.collection.php');
+	
+class Config extends Coll {
+	const TABLE_NAME = 'konkurranse_config';
+	const PARENT_FIELD = 'name';
+
+	public function getTableName() {
+		return self::TABLE_NAME;
+	}
+
+	public static function set( $name, $value ) {
+		try {
+			self::_create( [
+				'name' => $name,
+				'value' => $value,
+				]
+			);
+		} catch( Exception $e ) {
+			if( $e->getCode() == 901001 ) {
+				return self::update( $name, $value );
+			}
+			throw $e;
+		}
+		return $value;
+	}
+	
+	public static function update( $name, $value ) {
+		$sqlIns = new SQLins(
+			self::getTableName(),
+			[
+				'name' => $name
+			]
+		);
+		$sqlIns->add('value', $value );
+		$sqlIns->run();
+		return $value;
+	}
+
+	public function get( $name ) {
+		$sql = new SQL(
+			"SELECT * FROM `". self::TABLE_NAME ."` WHERE `name` = '#id' ",
+			['id' => $name]
+		);
+		return $sql->run('field', 'value');
+	}
+	
+}
---
+++ Konkurranse/sporsmal.collection.php
@@ -0,0 +1,27 @@
+<?php
+
+
+require_once('UKM/_orm.collection.php');
+require_once('UKM/Konkurranse/sporsmal.class.php');
+	
+class SporsmalColl extends Coll {
+	const TABLE_NAME = Sporsmal::TABLE_NAME;
+	public static $models = null;
+
+	public static function create( $name, $type, $answerType='tekst' ) {
+		$object = self::_create( [
+			'name' => $name,
+			'type' => $type,
+			'answer_type' => $answerType,
+			]
+		);
+		return $object;
+	}
+	
+	public function delete( $sporsmal ) {
+		self::_delete( [
+			'id' => $sporsmal->getId()
+			]
+		);
+	}
+}
