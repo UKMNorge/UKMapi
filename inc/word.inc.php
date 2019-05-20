21dac3a1678fa0879b92c59906cb0a39ada4c82b
Marius Mandal <mariusmandal@gmail.com>
2019-05-20 14:16:03 +0200

    Update word.inc.php

--- inc/word.inc.php
+++ inc/word.inc.php
@@ -46,31 +46,35 @@ function woWrite($filename){
 	return DOWNLOAD_URL_WORD . $filename;
 }
 
-function word_init($name,$orientation='portrait') {
+/**
+ * TODO: class.rapport.php burde bruke denne som utgangspunkt.
+ * Akkurat nå er denne en klone med motsatt parameter-rekkefølge.
+ * 
+ * 
+ */
+function word_init($name, $orientation='portrait') {
 	global $PHPWord;
 	$section = $PHPWord->createSection(array('orientation' => $orientation,
-		    'marginLeft' => 1100,
-		    'marginRight' => 1100,
-		    'marginTop' => 1100,
-		    'marginBottom' => 1100));
-	$properties = $PHPWord->getProperties();
+			'marginLeft' => 1100,
+			'marginRight' => 1100,
+			'marginTop' => 1100,
+			'marginBottom' => 1100));
+	$properties = $PHPWord->getDocInfo(); 
 	$properties->setCreator('UKM Norge'); 
 	$properties->setCompany('UKM Norges arrangørsystem');
 	$properties->setTitle('UKM '. ucfirst(str_replace('_',' ',$name)));
-	$properties->setDescription('Rapport generert fra UKM Norges arrangørsystem'); 
-	$properties->setCategory('UKM-rapporter');
-	$properties->setLastModifiedBy('UKM Norge arrangørsystem');
+	$properties->setDescription('Generert fra UKM Norges arrangørsystem'); 
+	$properties->setCategory('UKM');
+	$properties->setLastModifiedBy('UKM Norges arrangørsystem');
 	$properties->setCreated( time() );
 	$properties->setModified( time() );
 
 	// Definer noen styles
-
 	$PHPWord->addFontStyle('f_p', array('size'=>10));
 	$PHPWord->addParagraphStyle('p_p', array('spaceAfter'=>0, 'spaceBefore'=>0));
 	$PHPWord->addParagraphStyle('p_bold', array('spaceAfter'=>0, 'spaceBefore'=>0));
 	
 	$PHPWord->addParagraphStyle('p_center', array('spaceAfter'=>0, 'spaceBefore'=>0, 'align'=>'center'));
-	$PHPWord->addParagraphStyle('right', array('spaceAfter'=>0, 'spaceBefore'=>0, 'align'=>'right'));
 			
 	$PHPWord->addParagraphStyle('p_grp', array('align'=>'left', 'spaceAfter'=>300));
 	$PHPWord->addParagraphStyle('p_h1', array('align'=>'left', 'spaceAfter'=>100));
@@ -84,16 +88,20 @@ function word_init($name,$orientation='portrait') {
 	$PHPWord->addFontStyle('f_h4', array('size'=>10, 'align'=>'left', 'bold'=>true));
 	$PHPWord->addFontStyle('f_bold', array('bold'=>true, 'spaceAfter'=>0, 'spaceBefore'=>0));
 	
-	$PHPWord->addParagraphStyle('p_h1_center', array('align'=>'center', 'spaceAfter'=>100));
-	$PHPWord->addFontStyle('f_h1_center', array('size'=>16, 'align'=>'center', 'bold'=>true));
-
-
 	$PHPWord->addParagraphStyle('p_rapportIkonSpacer', array('spaceBefore'=>3000));
 	$PHPWord->addParagraphStyle('p_rapportIkonSpacerLandscape', array('spaceBefore'=>1500));
 	$PHPWord->addParagraphStyle('p_rapport', array('align'=>'center', 'spaceBefore'=>400));
 	$PHPWord->addParagraphStyle('p_place', array('align'=>'center'));
 	$PHPWord->addFontStyle('f_rapport', array('size'=>35, 'bold'=>true, 'color'=>'1e4a45'));
 	$PHPWord->addFontStyle('f_place', array('size'=>25, 'bold'=>true, 'color'=>'1e4a45'));
+	
+	# Offset ganges med 0.05, 360 = 18 i Word-avsnitt-stil.
+	$PHPWord->addParagraphStyle('p_diplom_navn', array('spaceAfter'=>0, 'spaceBefore'=>360, 'align'=>'center'));
+	$PHPWord->addParagraphStyle('p_diplom_mellom', array('spaceAfter'=>0, 'spaceBefore'=>0, 'align'=>'center'));
+	$PHPWord->addParagraphStyle('p_diplom_monstring', array('spaceAfter'=>0, 'spaceBefore'=>0, 'align'=>'center'));
+	$PHPWord->addFontStyle('f_diplom_navn', array('size'=>25, 'bold'=>true, 'color'=>'1e4a45'));
+	$PHPWord->addFontStyle('f_diplom_mellom', array('size'=>18, 'bold'=>false, 'color'=>'1e4a45'));
+	$PHPWord->addFontStyle('f_diplom_monstring', array('size'=>18, 'bold'=>true, 'color'=>'1e4a45'));
 
 	$PHPWord->addParagraphStyle('p_page_divider', array('spaceAfter'=>0, 'spaceBefore'=>0, 'align'=>'center'));
 	$PHPWord->addFontStyle('f_page_divider', array('size'=>30, 'bold'=>true, 'color'=>'f3776f'));
@@ -104,26 +112,36 @@ function word_init($name,$orientation='portrait') {
 		woText($section, '','rapportIkonSpacerLandscape');
 	else
 		woText($section, '','rapportIkonSpacer');
-	$section->addImage('/home/ukmno/public_html/wp-content/plugins/UKMrapporter/UKM_logo.png', array('width'=>300, 'height'=>164, 'align'=>'center'));
+	
+	$section->addImage(dirname( __FILE__ ) . '/img/UKM_logo.png', array('width'=>300, 'height'=>164, 'align'=>'center'));
 
 	woText($section, ucfirst(str_replace('_',' ',$name)), 'rapport');
-	woText($section, get_option('season'),'place');
+	woText($section, $this->m->g('pl_name').' ('.$this->m->g('season').')','place');
+	$t = $this->frontInfo();
+	if ($t) {
+		woText($section, ''); // Adds an empty line before contacts
+		if( is_array( $t ) ) {
+			woText($section, array_shift($t), 'diplom_mellom'); 
+			foreach ($t as $v) {
+				woText($section, $v, 'diplom_monstring');
+			}
+		}
+	}
 	$section->addPageBreak();
 	
-	// Add header
-	$header = $section->createHeader();
-	$HT = $header->addTable();
-	$HT->addRow(720);
-	if($orientation == 'landscape')
-		$HT->addCell(10000)->addText('UKM-rapporter : '.utf8_decode($name), array('align'=>'left'));
-	else
-		$HT->addCell(5000)->addText('UKM-rapporter : '.utf8_decode($name), array('align'=>'left'));
-	$HT->addCell(5000, array('align'=>'right'))->addText(date('d.m.Y H:i:s'), array('align'=>'right'), array('align'=>'right'));
-	// Add footer
-	$footer = $section->createFooter();
-	$footer->addPreserveText('Side {PAGE} av {NUMPAGES}.', array('align'=>'right'));
-
+	if(!isset($this->wordWithoutHeaders)) {
+		// Add header
+		$header = $section->createHeader();
+		$HT = $header->addTable();
+		$HT->addRow(720);
+		if($orientation == 'landscape')
+			$HT->addCell(10000)->addText('UKM-rapporter : '.utf8_decode($name), array('align'=>'left'));
+		else
+			$HT->addCell(5000)->addText('UKM-rapporter : '.utf8_decode($name), array('align'=>'left'));
+		$HT->addCell(5000, array('align'=>'right'))->addText(date('d.m.Y H:i:s'), array('align'=>'right'), array('align'=>'right'));
+		// Add footer
+		$footer = $section->createFooter();
+		$footer->addPreserveText('Side {PAGE} av {NUMPAGES}.', array('align'=>'right'));
+	}	
 	return $section;
 }
-
-?>
