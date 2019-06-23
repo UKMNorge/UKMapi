93b45fc3e3b0ff0d10381a52b2aed39f03f2eabb
root <root@ns1.ukm-dns.no>
2019-06-24 01:30:21 +0200

    Proper class names etc

--- RFID/monitoraccess.collection.php
+++ RFID/monitoraccess.collection.php
@@ -5,8 +5,8 @@ namespace UKMNorge\RFID;
 require_once('orm.collection.php');
 require_once('scanner.class.php');
 	
-class ScannerColl extends RFIDColl {
-	const TABLE_NAME = AccessMonitor::TABLE_NAME;
+class MonitorAccessColl extends RFIDColl {
+	const TABLE_NAME = MonitorAccess::TABLE_NAME;
 	public static $models = null;
 
 	public function getForSessionId( $session_id ) {
