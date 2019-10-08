4d2e2ce1b9570cc4ff8133219f36160ca227aa13
Marius Mandal <mariusmandal@gmail.com>
2019-10-07 19:21:55 -0700

    Fjernet ubrukte filer

--- inc/crypto.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* Compares two strings in constant time, to avoid timing attacks */
-function constant_time_compare($str1, $str2) {
-	$res = $str1 ^ $str2;
-	$ret = strlen($str1) ^ strlen($str2); //not the same length, then fail ($ret != 0)
-	for($i = strlen($res) - 1; $i >= 0; $i--) $ret += ord($res[$i]);
-	return !$ret;
-}
