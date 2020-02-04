ba4487f4b642b9e0244511acfdca835e40e621a1
Marius Mandal <mariusmandal@gmail.com>
2020-02-04 21:14:40 +0100

    Gjør det mulig å bruke twig-filter i andre twig-oppsett

---
+++ Twig/Definitions/Functions.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace UKMNorge\Twig\Definitions;
+
+class Functions {
+    /**
+     * TWIG-funksjon: GET()
+     * Hent $_GET-variabel
+     *
+     * @param String $GET_key
+     * @return void
+     */
+    public function GET($GET_key)
+    {
+        echo 'GET()';
+        if (isset($_GET[$GET_key])) {
+            return $_GET[$GET_key];
+        }
+        return false;
+    }
+}
