b82d1e352eb164b1d4947db005f611b4f3c5fe37
Marius Mandal <mariusmandal@gmail.com>
2020-04-23 23:40:36 +0200

    harNeste() og getNeste() gir neste upcoming sending

--- Filmer/UKMTV/Direkte/Sendinger.php
+++ Filmer/UKMTV/Direkte/Sendinger.php
@@ -8,6 +8,52 @@ use UKMNorge\Database\SQL\Query;
 
 class Sendinger extends Collection
 {
+
+    /**
+     * Hent neste sending
+     *
+     * @return Sending
+     * @throws Exception
+     */
+    public function getNeste() {
+        $neste_sending = null;
+        foreach( $this->getAll() as $sending ) {
+            // Er den ferdig, er den heller ikke den neste.
+            if( $sending->erFerdig() ) {
+                continue;
+            }
+            // Første sending i samlingen er alltid neste
+            if( is_null($neste_sending) ) {
+                $neste_sending = $sending;
+            } 
+            // Starter denne tidligere, er denne neste
+            elseif( $neste_sending->getStart() > $sending->getStart() ) {
+                $neste_sending = $sending;
+            }
+        }
+        if( is_null( $neste_sending ) ) {
+            throw new Exception(
+                'Det er ingen flere kommende sendinger',
+                144002
+            );
+        }
+        return $neste_sending;
+    }
+
+    /**
+     * Finnes det en upcoming sending?
+     *
+     * @return Bool
+     */
+    public function harNeste() {
+        try {
+            $this->getNeste();
+            return true;
+        } catch( Exception $e ) {
+        }
+        return false;
+    }
+
     /**
      * Hent alle sendinger for ett arrangement
      *
