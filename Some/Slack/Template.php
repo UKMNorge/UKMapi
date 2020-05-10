02ab0865b87e5c26d5d509fe7667df9db2eabe92
Marius Mandal <mariusmandal@gmail.com>
2020-05-10 21:25:45 +0200

    Template for kanalvelger

--- Some/Slack/Template.php
+++ Some/Slack/Template.php
@@ -3,12 +3,48 @@
 namespace UKMNorge\Some\Slack;
 
 use UKMNorge\Slack\Block\Composition\Markdown;
+use UKMNorge\Slack\Block\Composition\PlainText;
+use UKMNorge\Slack\Block\Element\MultiSelect;
 use UKMNorge\Slack\Block\Section;
+use UKMNorge\Slack\Option;
 use UKMNorge\Slack\Payload\PayloadInterface;
 use UKMNorge\Some\Forslag\Ide;
+use UKMNorge\Some\Kanaler\Kanaler;
 
 class Template
 {
+
+    /**
+     * Hent standard kanal-selector
+     *
+     * @param Markdown $tekst
+     * @param Array<Option> $initial_options
+     * @return Section
+     */
+    public static function getKanalSelector(Markdown $tekst, Array $initial_options = null) {
+        $select = new Section($tekst);
+        $kanaler = Kanaler::getAlle();
+        $options = [];
+        foreach( $kanaler->getAll() as $kanal ) {
+            $options[] = new Option(
+                MultiSelect::class, new PlainText( $kanal->getNavn() ), $kanal->getId()
+            );
+        }
+
+        $select->setAccessory(
+            new MultiSelect(
+                'channels',
+                $options,
+                new PlainText('Velg kanaler')
+            )
+        );
+
+        if( !is_null($initial_options)) {
+            $select->getAccessory()->setInitialOptions($initial_options);
+        }
+
+        return $select;
+    }
 
     /**
      * Forhåndsvisning av ønsket status
