130477599053fd753fb190c91b50d88f4b1ac6ec
Marius Mandal <mariusmandal@gmail.com>
2020-05-14 12:54:37 +0200

    viewsubmission enklere metadata

--- Slack/API/Response/Plugin/Transport/ViewSubmission.php
+++ Slack/API/Response/Plugin/Transport/ViewSubmission.php
@@ -4,13 +4,16 @@ namespace UKMNorge\Slack\API\Response\Plugin\Transport;
 
 use UKMNorge\Slack\API\Response\View;
 
-class ViewSubmission extends Transport implements TransportInterface {
-    
-    public $view;
+class ViewSubmission extends Transport implements TransportInterface
+{
 
-    public function _post_construct() {
-        $this->setId( $this->getData()->trigger_id );
-        $this->setView( 
+    public $view;
+    public $metadata;
+
+    public function _post_construct()
+    {
+        $this->setId($this->getData()->trigger_id);
+        $this->setView(
             new View(
                 $this->getData()->view->id,
                 $this->getData()->view->callback_id
@@ -18,15 +21,40 @@ class ViewSubmission extends Transport implements TransportInterface {
         );
     }
 
-    public function setView( View &$view ) {
+    public function setView(View &$view)
+    {
         $this->view = $view;
     }
 
-    public function getView() {
+    public function getView()
+    {
         return $this->view;
     }
 
-    public function getCallbackId() {
+    public function getCallbackId()
+    {
         return $this->getView()->getCallbackId();
+    }
+
+    /**
+     * Fetch one or all metadata value(s)
+     *
+     * @param String $key
+     * @return mixed
+     */
+    public function getMetadata(String $key = null)
+    {
+        if (is_null($this->metadata)) {
+            $this->metadata = json_decode($this->getView()->private_metadata);
+        }
+
+        if (is_null($key)) {
+            return $this->metadata;
+        }
+
+        if (!isset($this->metadata->$key)) {
+            return false;
+        }
+        return $this->metadata->$key;
     }
 }
