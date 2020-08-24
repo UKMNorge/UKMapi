dd4b7eb605d948c66dc16ad12d09267df9887b30
Marius Mandal <mariusmandal@gmail.com>
2020-08-24 16:58:40 +0200

    lagring støtter oauth2-data

--- Slack/App/Write.php
+++ Slack/App/Write.php
@@ -16,9 +16,12 @@ class Write {
 	public static function storeAPIAccessToken($data)
 	{
 		$sql = new Insert(UKMApp::TABLE);
-		$sql->add('team_id', $data->team_id);
-		$sql->add('team_name', $data->team_name);
-		$sql->add('access_token', $data->access_token);
+		$sql->add('team_id', $data->team->id);
+		$sql->add('team_name', $data->team->name);
+        $sql->add('access_token', $data->authed_user->access_token);
+        $sql->add('bot_id', $data->bot_user_id);
+        $sql->add('bot_access_token', $data->access_token);
+
 		$sql->add('data', json_encode($data));
 
 		$res = $sql->run();
