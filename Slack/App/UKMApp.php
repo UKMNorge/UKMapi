8713c0429bbe0fff6881d079293b6feaa99bb7f1
Marius Mandal <mariusmandal@gmail.com>
2020-08-24 16:06:50 +0200

    oh the buttons

--- Slack/App/UKMApp.php
+++ Slack/App/UKMApp.php
@@ -20,6 +20,11 @@ class UKMApp extends App
         return 'https://slack.' . UKM_HOSTNAME . '/auth/';
     }
 
+    public static function getButton()
+    {
+        return '<a href="https://slack.com/oauth/v2/authorize?client_id=8200366342.393237549925&scope=app_mentions:read,channels:join,channels:read,chat:write,chat:write.customize,chat:write.public,dnd:read,files:read,groups:read,im:history,im:read,im:write,incoming-webhook,mpim:history,mpim:read,reactions:read,reactions:write,remote_files:read,remote_files:share,remote_files:write,team:read,users.profile:read,users:read,users:read.email,users:write&user_scope=users.profile:read"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>';
+    }
+
     /**
      * Hent ønskede scopes
      *
@@ -67,5 +72,9 @@ class UKMApp extends App
         }
         App::setToken($token);
         return true;
+    }
+
+    public static function setBotTokenFromTeamId(String $team_id){
+
     }
 }
