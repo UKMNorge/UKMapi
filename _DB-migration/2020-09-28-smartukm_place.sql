7d0c15c42b636e11c228c43adb8255d275da0562
Marius Mandal <mariusmandal@gmail.com>
2020-09-28 12:47:56 +0200

    Bedre (flere) indexer på place

---
+++ _DB-migration/2020-09-28-smartukm_place.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `smartukm_place`
+ADD INDEX `pl_visible` (`pl_visible`),
+ADD INDEX `pl_registered` (`pl_registered`),
+ADD INDEX `pl_videresending` (`pl_videresending`),
+ADD INDEX `pl_subtype` (`pl_subtype`)
