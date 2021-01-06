f070ee76e826246dc5bd58fb2e7d6ba5f868e288
Marius Mandal <mariusmandal@gmail.com>
2021-01-06 17:05:59 +0100

    Formatering

--- Kommunikasjon/Mottaker.php
+++ Kommunikasjon/Mottaker.php
@@ -5,21 +5,23 @@ namespace UKMNorge\Kommunikasjon;
 use Exception;
 use UKMNorge\Database\SQL\Query;
 
-class Mottaker {
+class Mottaker
+{
     var $navn = null;
     var $epost = null;
     var $mobil = null;
 
     const BOGUS_MOBIL = [44444444, 99999999];
 
-    public function __construct( String $navn = null, String $epost = null, String $mobil = null) {
+    public function __construct(String $navn = null, String $epost = null, String $mobil = null)
+    {
         $this->navn = $navn;
         $this->epost = $epost;
 
         // Hvis det er angitt et ugyldig mobilnummer: stooooopp!
-        if( !is_null($mobil) && !static::erMobil($mobil)) {
+        if (!is_null($mobil) && !static::erMobil($mobil)) {
             throw new Exception(
-                $mobil .' er ikke et gyldig mobilnummer.',
+                $mobil . ' er ikke et gyldig mobilnummer.',
                 149001
             );
         }
@@ -33,8 +35,9 @@ class Mottaker {
      * @param String $navn
      * @return Mottaker
      */
-    public static function fraEpost( String $epost, String $navn=null) {   
-        return new Mottaker( $navn, $epost );
+    public static function fraEpost(String $epost, String $navn = null)
+    {
+        return new Mottaker($navn, $epost);
     }
 
     /**
@@ -44,10 +47,11 @@ class Mottaker {
      * @param String $navn
      * @return Mottaker
      */
-    public static function fraMobil( String $mobil, String $navn=null) {
+    public static function fraMobil(String $mobil, String $navn = null)
+    {
         $mobil = (int) static::cleanMobil($mobil);
 
-        return new Mottaker( $navn, null, $mobil);
+        return new Mottaker($navn, null, $mobil);
     }
 
     /**
@@ -58,9 +62,10 @@ class Mottaker {
      * @param Int $maxlength
      * @return String
      */
-    public static function clean(String $string, String $allow_pattern='A-Za-z0-9-', Int $maxlength = false) {
-        $string = preg_replace('/[^'.$allow_pattern.'.]/', '', strip_tags($string));
-        if($maxlength) {
+    public static function clean(String $string, String $allow_pattern = 'A-Za-z0-9-', Int $maxlength = false)
+    {
+        $string = preg_replace('/[^' . $allow_pattern . '.]/', '', strip_tags($string));
+        if ($maxlength) {
             return substr($string, 0, $maxlength);
         }
         return $string;
@@ -72,10 +77,11 @@ class Mottaker {
      * @param String $mobil
      * @return void
      */
-    public static function cleanMobil(String $mobil) {
-        
+    public static function cleanMobil(String $mobil)
+    {
+
         // REMOVE FACEBOOK 3 SPECIAL CHARS
-		if( strlen( $mobil )== 11 && (int)$mobil == 0 ) {
+        if (strlen($mobil) == 11 && (int)$mobil == 0) {
             $mobil = substr($mobil, 3);
         }
 
@@ -86,10 +92,10 @@ class Mottaker {
      * Hent mottakerens navn
      * 
      * @return String $navn
-     */ 
+     */
     public function getNavn()
     {
-        if( empty( $this->navn ) ) {
+        if (empty($this->navn)) {
             return $this->getEpost();
         }
         return $this->navn;
@@ -100,15 +106,16 @@ class Mottaker {
      *
      * @return Bool
      */
-    public function harNavn(){
-        return strlen( $this->getNavn() ) > 0;
+    public function harNavn()
+    {
+        return strlen($this->getNavn()) > 0;
     }
 
     /**
      * Hent mottakerens e-postadresse
      * 
      * @return String $epost
-     */ 
+     */
     public function getEpost()
     {
         return $this->epost;
@@ -118,10 +125,10 @@ class Mottaker {
      * Hent mottakerens mobilnummer
      * 
      * @return Int $mobilnummer
-     */ 
+     */
     public function getMobil()
     {
-        return (Int) $this->mobil;
+        return (int) $this->mobil;
     }
 
     /**
@@ -130,22 +137,23 @@ class Mottaker {
      * @param Int $mobil
      * @return bool
      */
-    public static function erMobil(Int $mobil) {
-		//if( !4-serien && !9-serien) {
-		if ( !(90000000 < $mobil && $mobil < 99999999) && !(40000000 < $mobil && $mobil < 50000000) ) {
-			return false;
+    public static function erMobil(Int $mobil)
+    {
+        //if( !4-serien && !9-serien) {
+        if (!(90000000 < $mobil && $mobil < 99999999) && !(40000000 < $mobil && $mobil < 50000000)) {
+            return false;
         }
 
         // Kjente dummy-nummer
-		if( in_array($mobil, static::BOGUS_MOBIL) ) {
+        if (in_array($mobil, static::BOGUS_MOBIL)) {
             return false;
         }
 
         // Ikke 8 siffer
-        if(empty($mobil) || $mobil == 0 || strlen((string)$mobil) != 8 ) {
+        if (empty($mobil) || $mobil == 0 || strlen((string)$mobil) != 8) {
             return false;
         }
 
         return true;
-	}
+    }
 }
