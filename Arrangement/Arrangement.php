2710e694c446a5941b9a868e9c3e46d10f5a07a1
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-07-06 14:44:21 +0200

    Små fix

--- Arrangement/Arrangement.php
+++ Arrangement/Arrangement.php
@@ -1789,7 +1789,7 @@ class Arrangement
      */
     public function erDeltakereSynlig() {
         $ret = $this->getMetaValue('deltakeresynlig');
-        return $ret == null ? true : $ret;
+        return $ret == true;
     }
 
     /**
