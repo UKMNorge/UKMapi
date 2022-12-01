c8b8dd149d674e790f156de73f06654bee217811
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-12-01 15:55:59 +0100

    Get innslag på FeedbackDelta

--- Feedback/FeedbackDelta.php
+++ Feedback/FeedbackDelta.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace UKMNorge\Feedback;
-
+use UKMNorge\Database\SQL\Query;
+use UKMNorge\Innslag\Innslag;
+use Exception;
 
 class FeedbackDelta extends Feedback {
     private Int $platform = 1;
@@ -33,5 +35,35 @@ class FeedbackDelta extends Feedback {
      */
     public function saveMedInnslag(Int $innslag_id) {
         Write::saveFeedbackWithInnslag($this, $innslag_id);
+    }
+
+   /**
+     * Get innslag i Feedbacken
+     * 
+     * HUSK: Denne kan returnere null hvis innslaget finnes ikke eller hvis det skjer noe feil
+     * 
+     * 
+     * @return Innslag|null
+     **/
+    public function getInnslag() : Innslag {   
+        $SQL = new Query(
+            "SELECT rel_innslag_feedback.b_id
+            FROM rel_innslag_feedback
+            WHERE rel_innslag_feedback.feedback_id = '#feedback_id'
+            LIMIT 1",
+            [
+                'feedback_id' => $this->id
+            ]
+        );
+
+        try {
+            $res = Query::fetch($SQL->run());
+            if(!$res) return null;
+
+            return Innslag::getById($res['b_id']);
+        } catch(Exception $e) {
+            return null;
+        }
+        
     }
 }
