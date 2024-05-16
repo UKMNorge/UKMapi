f435b759ccf16e4ec1c55b4f83eac6f23f1a3213
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-05-16 15:27:26 +0200

    En klasse for å søke på forskjellige deler av arr.sys

---
+++ SearchArrangorsystemet/Search.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace UKMNorge\SearchArrangorsystemet;
+use UKMNorge\Nettverk\Administrator;
+
+use UKMNorge\Database\SQL\Query;
+
+require_once('UKM/Autoloader.php');
+
+class Search {
+    // Brukere kan søke på arrangementer, innslag, blogginnlegg, og brukere
+    public static function sokBlogs(String $searchInput) {
+        $blogs = get_blogs_of_user(get_current_user_id());
+        $retBlogs = [];
+        // Search for blogname
+        foreach ($blogs as $blog) {
+            if(static::searchText($blog->blogname, $searchInput)) {
+                $blog_id = $blog->userblog_id; // Get the blog ID
+                switch_to_blog($blog_id); // Switch to the context of the current blog
+                $retBlogs[] = [
+                    'site_id' => $blog->site_id,
+                    'userblog_id' => $blog->userblog_id,
+                    'title' => $blog->blogname,
+                    'siteUrl' => $blog->siteurl,
+                    'site_type' => get_option('site_type'),
+                    'pl_id' => get_option('pl_id'),
+                    'kommune' => get_option('kommune'),
+                ];
+                restore_current_blog();
+            }
+        }   
+
+        return $retBlogs;
+    }
+    
+    public static function searchOmraader($searchInput) {
+        $retOmrader = [];
+
+        $current_admin = new Administrator(get_current_user_id());
+        $omrader = $current_admin->getOmrader();
+        foreach($omrader as $omrade) {
+            if(static::searchText($omrade->getNavn(), $searchInput)) {
+                $retOmrader[] = [
+                    'id' => $omrade->getId(),
+                    'navn' => $omrade->getNavn(),
+                    'type' => $omrade->getType(),
+                    'siteUrl' => $omrade->getLink(),
+                ];
+            }
+        }
+
+        return $retOmrader;
+    }
+
+    public function sokDeltakere() {
+    
+    }
+
+    private static function searchText($entitytext, $searchInput) : bool {
+        $searchThreshold = 3;
+        $distance = levenshtein($searchInput, $entitytext);
+
+        return $distance < $searchThreshold;
+    }
+}
+
