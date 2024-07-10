57064103390ae0a16a2f33ef831d2cc0b1f1c412
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-07-10 19:59:42 +0200

    StatistikkArrangement endringer
    Tilpassing og endringer

--- Statistikk/Objekter/StatistikkArrangement.php
+++ Statistikk/Objekter/StatistikkArrangement.php
@@ -5,24 +5,45 @@ namespace UKMNorge\Statistikk\Objekter;
 use UKMNorge\Arrangement\Arrangement;
 use UKMNorge\Database\SQL\Query;
 use UKMNorge\Statistikk\Objekter\StatistikkSuper;
-
+use UKMNorge\Statistikk\StatistikkManager;
+use Exception;
 
 class StatistikkArrangement extends StatistikkSuper {
-    private $arrangement;
+    private Arrangement $arrangement;
+    private StatistikkManager $sm;
+
 
     public function __construct(Arrangement $arrangement) {
+        $this->sm = new StatistikkManager();
         // Check if the user has access to the arrangement
+        if($this->sm::hasAccessToArrangement($arrangement) == false) {
+            throw new Exception('Ingen tilgang til arrangement ' . $arrangement->getId(), 401);
+        }
         $this->arrangement = $arrangement;
     }
 
     /**
-    * Returnerer antall deltakere i arrangementet fra database.
+    * Returnerer antall unike deltakere i arrangementet
     *
-    * @return int The number of participants.
+    * @return int antall unike deltakere.
+    */
+    public function getAntallUnikeDeltakere() : int {
+        return $this->runAntall(true);
+    }
+
+    /**
+    * Returnerer antall IKKE UNIKE deltakere i arrangementet
+    *
+    * @return int antall deltakere.
     */
     public function getAntallDeltakere() : int {
+        return $this->runAntall();
+    }
+
+    private function runAntall($unique = false) : int {
+        $select = $unique ? "COUNT(DISTINCT p_id)" : "COUNT(p_id)";
         $sql = new Query(
-            "SELECT COUNT(*) as antall
+            "SELECT " . $select . " as antall
             FROM (
                 " . $this->getQueryArrangement($this->arrangement) . "
             ) AS subquery;",
@@ -33,6 +54,7 @@ class StatistikkArrangement extends StatistikkSuper {
 
         $res = $sql->run('array');
         return (int) intval($res['antall']);
+
     }
 
 }
