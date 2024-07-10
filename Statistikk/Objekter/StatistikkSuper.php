fecabeee6e14bff339bf8c28f8984c3fbf503921
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-07-10 20:00:03 +0200

    Implementering av kommune query

--- Statistikk/Objekter/StatistikkSuper.php
+++ Statistikk/Objekter/StatistikkSuper.php
@@ -4,7 +4,7 @@ namespace UKMNorge\Statistikk\Objekter;
 
 use UKMNorge\Arrangement\Arrangement;
 use UKMNorge\Database\SQL\Query;
-
+use UKMNorge\Geografi\Kommune;
 
 class StatistikkSuper {
     public function __construct() {
@@ -12,8 +12,9 @@ class StatistikkSuper {
     }
 
     protected function getQueryArrangement(Arrangement $arrangement) : String {
+        $retQuery = '';
         if($arrangement->getSesong() > 2019) {
-            return "SELECT * 
+            $retQuery = "SELECT person_id as p_id, innslag_id as b_id
                 FROM `statistics_before_2024_ukm_rel_arrangement_person`
                 JOIN `statistics_before_2024_smartukm_band` AS `innslag`
                 ON `innslag`.`b_id` = `statistics_before_2024_ukm_rel_arrangement_person`.`innslag_id`
@@ -21,16 +22,58 @@ class StatistikkSuper {
                 AND `b_status` = 8";
         }
         else {
-            return "SELECT pl_b.pl_id, b.b_id, b.b_name, b_p.p_id, p.p_firstname, arrangement.pl_type
+            $retQuery = "SELECT b_p.p_id, b.b_id
                 FROM statistics_before_2024_smartukm_rel_pl_b AS pl_b
                 JOIN statistics_before_2024_smartukm_rel_b_p AS b_p ON b_p.b_id = pl_b.b_id
                 JOIN statistics_before_2024_smartukm_band AS b ON b.b_id = b_p.b_id
                 JOIN statistics_before_2024_smartukm_place arrangement ON arrangement.pl_id = pl_b.pl_id
                 JOIN statistics_before_2024_smartukm_participant AS p ON p.p_id = b_p.p_id
-                WHERE pl_b.pl_id=#plId 
+                WHERE pl_b.pl_id='#plId' 
                 AND (b.b_status = 8 OR b.b_status = 99)
                 GROUP BY b_p.b_id, b_p.p_id";
         }
+
+        // If arrangementet er fra 2024
+        if($arrangement->getSesong() > 2023) {
+            $retQuery .= " UNION SELECT p_id, b_id
+            FROM ukm_statistics_from_2024
+            WHERE pl_id='#plId'";
+        }
+
+        return $retQuery;
+    }
+    
+    protected function getQueryKommune(Kommune $kommune, int $season) : String {
+        $retQuery = '';
+        if($season > 2019) {
+            $retQuery = "SELECT person_id as p_id, innslag_id as b_id
+                FROM statistics_before_2024_smartukm_rel_pl_k AS arr_kommune
+                JOIN statistics_before_2024_smartukm_place AS arrangement ON arrangement.pl_id=arr_kommune.pl_id
+                JOIN statistics_before_2024_ukm_rel_arrangement_person AS arr_person ON arr_person.arrangement_id=arrangement.pl_id
+                JOIN statistics_before_2024_smartukm_band AS innslag ON innslag.b_id=arr_person.innslag_id
+                WHERE arrangement.season='#season' AND k_id='#k_id' AND innslag.b_status = 8";
+        }
+
+        else {
+            $retQuery = "SELECT p_id, arr_innslag.b_id as b_id
+            FROM statistics_before_2024_smartukm_rel_pl_k AS arr_kommune
+            JOIN statistics_before_2024_smartukm_place AS arrangement ON arrangement.pl_id=arr_kommune.pl_id
+            JOIN statistics_before_2024_smartukm_rel_pl_b AS arr_innslag ON arr_innslag.pl_id=arrangement.pl_id
+            JOIN statistics_before_2024_smartukm_rel_b_p AS innslag_person ON innslag_person.b_id = arr_innslag.b_id
+            JOIN statistics_before_2024_smartukm_band AS innslag ON innslag.b_id=arr_innslag.b_id
+            WHERE arr_kommune.`k_id`='#k_id' AND arrangement.season='#season' AND (innslag.b_status = 8 OR innslag.b_status = 99)
+            GROUP BY arr_innslag.b_id, p_id";
+        }
+
+        // If season er fra 2024
+        if($season > 2023) {
+            $retQuery .= " UNION SELECT p_id, b_id
+            FROM ukm_statistics_from_2024
+            WHERE k_id='#k_id' AND season='#season";
+        }
+
+        return $retQuery;
+        
     }
 
 }
