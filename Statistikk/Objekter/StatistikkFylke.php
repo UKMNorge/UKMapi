91ff2f4b7c0edc0ed35b41de7db6950e178132be
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-07-11 16:57:09 +0200

    Legger til metoder for å hente deltakere på fylke

--- Statistikk/Objekter/StatistikkFylke.php
+++ Statistikk/Objekter/StatistikkFylke.php
@@ -20,4 +20,46 @@ class StatistikkFylke extends StatistikkSuper {
         $this->season = $season;
     }
 
+    /**
+    * Returnerer antall unike deltakere i fylke
+    *
+    * @return int antall unike deltakere.
+    */
+    public function getAntallUnikeDeltakere() : int {
+        return $this->runAntall(true);
+    }
+
+    /**
+    * Returnerer antall IKKE UNIKE deltakere i fylke
+    *
+    * @return int antall deltakere.
+    */
+    public function getAntallDeltakere() : int {
+        return $this->runAntall();
+    }
+
+
+    private function runAntall($unique = false) : int {
+        $select = $unique ? "COUNT(DISTINCT p_id)" : "COUNT(p_id)";
+        $sql = new Query(
+            "SELECT " . $select . " as antall
+            FROM (
+                " . $this->getQueryFylke($this->season) . "
+            ) AS subquery;",
+            [
+                'fylke_id' => $this->fylke->getId(),
+                'season' => $this->season
+            ]
+        );   
+
+        $res = $sql->run('array');
+        return (int) intval($res['antall']);
+    }
+
+
+
+
+
+
+   
 }
