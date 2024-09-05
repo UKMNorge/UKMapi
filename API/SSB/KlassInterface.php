64839f7e932586dcded42bc9733afaf113fb2736
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-09-05 21:38:18 +0200

    http støttes ikke lenger. Bruker https

--- API/SSB/KlassInterface.php
+++ API/SSB/KlassInterface.php
@@ -4,7 +4,7 @@ namespace UKMNorge\API\SSB;
 
 interface KlassInterface {
 
-	const API_URL = 'http://data.ssb.no/api/klass/v1/classifications/';
+	const API_URL = 'https://data.ssb.no/api/klass/v1/classifications/';
 
 	# Denne funksjonen velger hvilken SSB-ressurs spørringen skal kjøre mot (oftest en tabell).
 	# Argument må være på formen 'ressurs/ressurs-id', ie 'table/04231' for Levendefødte.
