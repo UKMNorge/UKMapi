67bcb2d5e8a0ae898870faf755a0637c60b474d4
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-10-21 18:05:40 +0200

    Endringer på AldersfordelingSSB med mulighet for å hente alle personer i kommunen

--- Statistikk/Objekter/StatistikkSSB.php
+++ Statistikk/Objekter/StatistikkSSB.php
@@ -6,28 +6,18 @@ use Exception;
 class StatistikkSSB {
 
 
-    /**
-     * Henter aldersfordeling for en kommune fra SSB API
-     *
-     * Alder fra 10 til 25 blir hentet for menn og kvinner som er fusjonert i en enkelt array.
-     * 
-     * @param int $kommuneId
-     * @param int $season
-     * @return void
-     */
-    public static function getAldersfordelingKommune($kommuneId, int $season) : array {
-        // URL til SSB API for Statistikkbanken
+    public static function getAldersfordelingKommune($kommuneId, int $season): array {
         $url = "https://data.ssb.no/api/v0/no/table/07459";
-
-        // JSON query med spesifikk kommune
-        $query = [
+    
+        // JSON query for age distribution
+        $queryAgeDistribution = [
             "query" => [
                 [
                     "code" => "Region",
                     "selection" => [
                         "filter" => "item",
                         "values" => [
-                            $kommuneId,  // kommune id
+                            $kommuneId,
                         ]
                     ]
                 ],
@@ -36,8 +26,8 @@ class StatistikkSSB {
                     "selection" => [
                         "filter" => "item",
                         "values" => [
-                            "1",  // Menn
-                            "2"   // Kvinner
+                            "1", // Men
+                            "2"  // Women
                         ]
                     ]
                 ],
@@ -45,24 +35,8 @@ class StatistikkSSB {
                     "code" => "Alder",
                     "selection" => [
                         "filter" => "item",
-                        // Alder
                         "values" => [
-                            "010",
-                            "011",
-                            "012",
-                            "013",
-                            "014",
-                            "015",
-                            "016",
-                            "017",
-                            "018",
-                            "019",
-                            "020",
-                            "021",
-                            "022",
-                            "023",
-                            "024",
-                            "025",
+                            "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021"
                         ]
                     ]
                 ],
@@ -71,7 +45,7 @@ class StatistikkSSB {
                     "selection" => [
                         "filter" => "item",
                         "values" => [
-                            "Personer1"  // Antall personer
+                            "Personer1" // Number of persons
                         ]
                     ]
                 ],
@@ -80,7 +54,7 @@ class StatistikkSSB {
                     "selection" => [
                         "filter" => "item",
                         "values" => [
-                            $season  // Sesong
+                            $season
                         ]
                     ]
                 ]
@@ -89,14 +63,77 @@ class StatistikkSSB {
                 "format" => "json-stat2"
             ]
         ];
-
-        // Konverterer query til JSON-format
+    
+        // Add another query to get total population data
+        $queryTotalPopulation = [
+            "query" => [
+                [
+                    "code" => "Region",
+                    "selection" => [
+                        "filter" => "item",
+                        "values" => [
+                            $kommuneId,
+                        ]
+                    ]
+                ],
+                [
+                    "code" => "Kjonn",
+                    "selection" => [
+                        "filter" => "item",
+                        "values" => [
+                            "1", // Men
+                            "2"  // Women
+                        ]
+                    ]
+                ],
+                [
+                    "code" => "Alder",
+                    "selection" => [
+                        "filter" => "all",
+                        "values" => []
+                    ]
+                ],
+                [
+                    "code" => "ContentsCode",
+                    "selection" => [
+                        "filter" => "item",
+                        "values" => [
+                            "Personer1" // Number of persons
+                        ]
+                    ]
+                ],
+                [
+                    "code" => "Tid",
+                    "selection" => [
+                        "filter" => "item",
+                        "values" => [
+                            $season
+                        ]
+                    ]
+                ]
+            ],
+            "response" => [
+                "format" => "json-stat2"
+            ]
+        ];
+    
+        // Function to perform cURL request
+        $ageData = self::performCurlRequest($url, $queryAgeDistribution);
+        // $totalPopulationData = self::performCurlRequest($url, $queryTotalPopulation);
+    
+        // Process the data for age distribution
+        $ageCounts = self::processAgeDistribution($ageData);
+        // $totalPopulation = self::processTotalPopulation($totalPopulationData);
+    
+        return [
+            'age_distribution' => $ageCounts,
+            'total_population' => 0
+        ];
+    }
+    
+    private static function performCurlRequest($url, $query): array {
         $jsonQuery = json_encode($query);
-
-        // Initialiserer en cURL-session
         $ch = curl_init($url);
-
-        // Setter cURL-innstillinger for å sende en POST-forespørsel med JSON-data
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             'Content-Type: application/json',
@@ -104,50 +141,46 @@ class StatistikkSSB {
         ]);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonQuery);
-
-        // Utfører forespørselen og henter responsen
+    
         $response = curl_exec($ch);
-
-        // Sjekker om cURL-forespørselen var vellykket
         if ($response === false) {
             throw new Exception("cURL error: " . curl_error($ch));
         }
-
-        // Dekoder JSON-responsen
+    
         $data = json_decode($response, true);
-
-        // Sjekk for feil i responsen
+        curl_close($ch);
+    
         if (isset($data['error'])) {
             throw new Exception("SSB API error: " . $data['error']['message']);
         }
-
-        // Henter dimensjoner
+    
+        return $data;
+    }
+    
+    private static function processAgeDistribution($data): array {
         $dimensions = $data['dimension'];
         $values = $data['value'];
-
-        // Henter indekser for dimensjoner
-        $ageIndex = $dimensions['Alder']['category']['index'];
+    
         $ageLabels = $dimensions['Alder']['category']['label'];
-        $ageSize = count($ageLabels);
-
-        // Beregner antall personer for hver alder
         $ageCounts = array_fill_keys(array_keys($ageLabels), 0);
-
-        $indexSize = $dimensions['Region']['size'][0] * $dimensions['Kjonn']['size'][0] * $dimensions['Alder']['size'][0] * $dimensions['Tid']['size'][0];
+    
+        $ageSize = count($ageLabels);
         for ($i = 0; $i < count($values); $i++) {
             $ageIdx = ($i % $ageSize);
             $ageKey = array_keys($ageLabels)[$ageIdx];
             $ageCounts[$ageKey] += $values[$i];
         }
-
+    
         $retArr = [];
         foreach ($ageCounts as $ageCode => $count) {
             $retArr[intval($ageCode)] = ['age' => intval($ageCode), 'antall' => $count];
         }
-
-        // Lukker cURL-sessionen
-        curl_close($ch);
-
+    
         return $retArr;
     }
+    
+    private static function processTotalPopulation($data): int {
+        return array_sum($data['value']);
+    }
+    
 }
