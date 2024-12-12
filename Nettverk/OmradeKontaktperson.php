8fdc12a0b6f4658da45b4350602f8980c14cb2e4
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-12-12 23:51:17 +0100

    createEmpty og getArray på OmradeKontaktperson

--- Nettverk/OmradeKontaktperson.php
+++ Nettverk/OmradeKontaktperson.php
@@ -16,7 +16,8 @@ class OmradeKontaktperson {
     private DateTime $created_date; // Read only - set by database
     private DateTime $modified_date; // Read only - edited by database
     private int $eier_omrade_id;
-    private int $eier_omrade_type;
+    private string $eier_omrade_type;
+    private int|null $wp_user_id;
 
     public function __construct(array $row) {
         $this->id = $row['id'];
@@ -27,6 +28,7 @@ class OmradeKontaktperson {
         $this->epost = $row['epost'];
         $this->eier_omrade_id = $row['eier_omrade_id'];
         $this->eier_omrade_type = $row['eier_omrade_type'];
+        $this->wp_user_id = $row['wp_user_id'] ?? null;
 
         if( !empty($row['created_date']) ) {
             $this->created_date = new DateTime( $row['created_date'] );
@@ -34,6 +36,35 @@ class OmradeKontaktperson {
         if( !empty($row['modified_date']) ) {
             $this->modified_date = new DateTime( $row['modified_date'] );
         }
+    }
+
+    public static function createEmpty() : OmradeKontaktperson {
+        return new OmradeKontaktperson([
+            'id' => -1,
+            'fornavn' => '',
+            'etternavn' => '',
+            'mobil' => '',
+            'epost' => '',
+            'beskrivelse' => '',
+            'eier_omrade_id' => -1,
+            'eier_omrade_type' => '',
+        ]);
+    }
+
+    public function getArray() {
+        return [
+            'id' => $this->id,
+            'fornavn' => $this->fornavn,
+            'etternavn' => $this->etternavn,
+            'mobil' => $this->mobil,
+            'beskrivelse' => $this->beskrivelse,
+            'epost' => $this->epost,
+            'created_date' => $this->created_date->format('Y-m-d H:i:s'),
+            'modified_date' => $this->modified_date->format('Y-m-d H:i:s'),
+            'eier_omrade_id' => $this->eier_omrade_id,
+            'eier_omrade_type' => $this->eier_omrade_type,
+            'wp_user_id' => $this->wp_user_id,
+        ];
     }
 
     public function getId() {
@@ -76,18 +107,26 @@ class OmradeKontaktperson {
         return $this->beskrivelse;
     }
 
-    public function setBeskrivelse(string $beskrivelse) {
-        $this->beskrivelse = $beskrivelse;
+    public function setBeskrivelse(string|null $beskrivelse) {
+        $this->beskrivelse = $beskrivelse ?? '';
     }
 
     public function getEpost() {
         return $this->epost;
     }
 
-    public function setEpost(string $epost) {
-        $this->epost = $epost;
+    public function setEpost(string|null $epost) {
+        $this->epost = $epost ?? '';
+    }
+
+    public function getWpUserId() {
+        return $this->wp_user_id;
     }
     
+    public function setWpUserId(int $wp_user_id) {
+        $this->wp_user_id = $wp_user_id;
+    }
+
     // Read only
     public function getCreatedDate() {
         return $this->created_date;
