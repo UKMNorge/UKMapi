2e8d1583f33a775c25bb21fc97681d56ac3a90ad
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-10-26 16:38:55 +0100

    Ny klasse for å representere interesse
    Melding av interesse skal håndteres av denne klassen. Brukere i utgangspunktet uten autentisering kan melde interesse.
    
    HUSK å kjøre DB spørring:
    CREATE TABLE smartukm_interesse(
      `id`                 INT NOT NULL AUTO_INCREMENT,
      `navn`               VARCHAR(255)      NOT NULL,
      `beskrivelse`        TEXT,
      `epost`              VARCHAR(255),
      `mobil`              VARCHAR(32),
      `arrangor_interesse` TINYINT(1)        NOT NULL DEFAULT 0,
      PRIMARY KEY (`id`)
     )

---
+++ Interesse/Interesse.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace UKMNorge\Interesse;
+
+use Exception;
+
+require_once('UKM/Autoloader.php');
+
+
+class Interesse {
+    private int $id;
+    private string $navn;
+    private string $beskrivelse;
+    private string $epost;
+    private string $mobil;
+    private bool $arrangorInteresse;
+    private array $kommuner = [];
+
+
+    public function __construct($id, $navn, $beskrivelse, $epost, $mobil, $arrangorInteresse, $kommuner = []) {
+        $this->id = $id;
+        $this->navn = $navn;
+        $this->beskrivelse = $beskrivelse;
+        $this->epost = $epost;
+        $this->mobil = $mobil;
+        $this->arrangorInteresse = $arrangorInteresse;
+        $this->kommuner = $kommuner;
+    }
+
+    public function getId() : int {
+        return $this->id;
+    }
+
+    public function getNavn() : string {
+        return $this->navn;
+    }
+
+    public function getBeskrivelse() : string {
+        return $this->beskrivelse;
+    }
+
+    public function getEpost() : string {
+        return $this->epost;
+    }
+
+    public function getMobil() : string {
+        return $this->mobil;
+    }
+
+    public function isArrangorInteresse() : bool {
+        return $this->arrangorInteresse;
+    }
+
+    public function getKommuner() : array {
+        return $this->kommuner;
+    }
+}
