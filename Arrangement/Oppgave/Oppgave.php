89e122806e23f74ca47282bbef5114a7f48f4b3e
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2026-05-19 21:02:09 +0200

    getOppgaveBesvartStatus på nominasjon

--- Arrangement/Oppgave/Oppgave.php
+++ Arrangement/Oppgave/Oppgave.php
@@ -233,6 +233,9 @@ class Oppgave {
 
     public function getOppgaveBesvartStatusByMobil($mobil): int {
         $deltaUserId = $this->getDeltaUserIdByMobil($mobil);
+        if($deltaUserId === -1) {
+            return -1;
+        }
         $personIds = $this->getPersonIdsByMobil($mobil);
         $maxStatus = 0;
         foreach ($personIds as $personId) {
@@ -265,6 +268,6 @@ class Oppgave {
             'ukmdelta'
         );
         $res = $sql->run('array');
-        return $res['id'];
+        return $res['id'] ?? -1;
     }
 }
